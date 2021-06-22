3406f82d5155f6c481526331464c1a5a945da2b9
Kostiantyn <konkos2505@gmail.com>
2021-06-22 14:19:41 +0300

    first commit

---
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -0,0 +1,58 @@
+package mate.academy.spring.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.MovieSessionRequestDto;
+import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.service.MovieSessionService;
+import mate.academy.spring.service.mapper.MovieSessionDtoMapper;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie-sessions")
+public class MovieSessionController {
+    private final MovieSessionService movieSessionService;
+    private final MovieSessionDtoMapper movieSessionDtoMapper;
+
+    public MovieSessionController(MovieSessionService movieSessionService,
+                                  MovieSessionDtoMapper movieSessionDtoMapper) {
+        this.movieSessionService = movieSessionService;
+        this.movieSessionDtoMapper = movieSessionDtoMapper;
+    }
+
+    @PostMapping
+    public Long add(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+        return movieSessionService.add(movieSessionDtoMapper.parseToModel(movieSessionRequestDto))
+                .getId();
+    }
+
+    @GetMapping("/available")
+    public List<MovieSessionResponseDto> getAllAvailable(
+            @RequestParam Long id, @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate date) {
+        return movieSessionService.findAvailableSessions(id, date).stream()
+                .map(movieSessionDtoMapper::parseToDto)
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping("/${id}")
+    public MovieSession update(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+        return movieSessionService.update(movieSessionDtoMapper.parseToModel(
+                movieSessionRequestDto));
+    }
+
+    @DeleteMapping("/${id}")
+    public void delete(@PathVariable Long id) {
+        movieSessionService.delete(id);
+    }
+}
