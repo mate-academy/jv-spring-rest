d91f863ae30b19db67dac08974a557028215e8f5
Maksim <max.freedman.main@gmail.com>
2021-06-22 08:39:54 +0300

    Added movie session controller impl

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -1,9 +1,10 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
 import mate.academy.spring.service.MovieSessionService;
-import mate.academy.spring.service.mapper.MovieDtoMapper;
+import mate.academy.spring.service.mapper.MovieSessionDtoMapper;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,25 +14,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/movie-sessions")
 public class MovieSessionController {
     private final MovieSessionService movieSessionService;
+    private final MovieSessionDtoMapper movieSessionDtoMapper;
 
-    public MovieSessionController(MovieSessionService movieSessionService) {
+    public MovieSessionController(MovieSessionService movieSessionService,
+                                  MovieSessionDtoMapper movieSessionDtoMapper) {
         this.movieSessionService = movieSessionService;
+        this.movieSessionDtoMapper = movieSessionDtoMapper;
     }
 
     @PostMapping
     public MovieSessionResponseDto create(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
-        return null;
+        MovieSession movieSessionModel = movieSessionDtoMapper.toModel(movieSessionRequestDto);
+        MovieSession movieSessionFromDb = movieSessionService.add(movieSessionModel);
+        return movieSessionDtoMapper.parse(movieSessionFromDb);
     }
 
-    @GetMapping("/available")
-    public MovieSessionResponseDto getAllByMovieAndDate(@RequestParam Long movieId,
-                                                        @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy")
+    @GetMapping(path = "/available")
+    public List<MovieSessionResponseDto> getAllByMovieAndDate(@RequestParam Long movieId,
+                                                              @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy")
                                                                 LocalDate date) {
-        return null;
+        return movieSessionService.findAvailableSessions(movieId, date)
+                .stream()
+                .map(movieSessionDtoMapper::parse)
+                .collect(Collectors.toList());
     }
 }
