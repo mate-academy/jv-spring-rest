5201d1429cf3c7f2c508885d7e7d88a1e2b35eb1
vuyko23 <mykolasakulych@gmail.com>
2021-06-22 15:14:34 +0300

    Rest Solution

---
+++ src/main/java/mate/academy/spring/dto/CinemaResponseHallDto.java
@@ -0,0 +1,10 @@
+package mate.academy.spring.dto;
+
+import lombok.Data;
+
+@Data
+public class CinemaResponseHallDto {
+    private Long id;
+    private int capacity;
+    private String description;
+}
---
+++ src/main/java/mate/academy/spring/dto/MovieRequestDto.java
@@ -0,0 +1,9 @@
+package mate.academy.spring.dto;
+
+import lombok.Data;
+
+@Data
+public class MovieRequestDto {
+    private String title;
+    private String description;
+}
