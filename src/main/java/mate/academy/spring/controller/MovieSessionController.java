68c06a81b7c03c61a4fb57df1495438341c37083
Maksim <max.freedman.main@gmail.com>
2021-06-22 07:49:33 +0300

    Added movie session controller

---
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.service.MovieSessionService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/movie-sessions")
+public class MovieSessionController {
+    private final MovieSessionService movieSessionService;
+
+    public MovieSessionController(MovieSessionService movieSessionService) {
+        this.movieSessionService = movieSessionService;
+    }
+
+    @PostMapping
+    public MovieSessionResponseDto create(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+        return null;
+    }
+
+    @GetMapping("/available")
+    public MovieSessionResponseDto getAllByMovieAndDate(@RequestParam Long movieId,
+                                                        @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy")
+                                                                LocalDate date) {
+        return null;
+    }
+}
