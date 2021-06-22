bd515f3ab82dea5afc7f9ed76712bcd98c47c1a4
Ihor-Nykyforov <kifa1203@gmail.com>
2021-06-22 14:37:37 +0300

    Reworked method delete in MovieSessionDao

--- src/main/java/mate/academy/spring/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/MovieSessionDaoImpl.java
@@ -78,19 +78,19 @@ public class MovieSessionDaoImpl extends AbstractDao<MovieSession> implements Mo
     }
 
     @Override
-    public void delete(Long id) {
+    public void delete(MovieSession movieSession) {
         Session session = null;
         Transaction transaction = null;
         try {
             session = sessionFactory.openSession();
             transaction = session.beginTransaction();
-            session.delete(get(id).get());
+            session.delete(movieSession);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't delete movie session by id: " + id, e);
+            throw new DataProcessingException("Can't delete movie session " + movieSession, e);
         } finally {
             if (session != null) {
                 session.close();
