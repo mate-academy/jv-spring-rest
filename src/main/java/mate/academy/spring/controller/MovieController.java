747bb7f61bc65955bea84b8e7481c5bd11895b30
Maksim <max.freedman.main@gmail.com>
2021-06-22 07:43:05 +0300

    Added movie controller impl

--- src/main/java/mate/academy/spring/controller/MovieController.java
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -1,30 +1,42 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.dto.MovieRequestDto;
 import mate.academy.spring.model.dto.MovieResponseDto;
 import mate.academy.spring.service.MovieService;
+import mate.academy.spring.service.mapper.MovieDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
     private final MovieService movieService;
+    private final MovieDtoMapper movieDtoMapper;
 
-    public MovieController(MovieService movieService) {
+    public MovieController(MovieService movieService, MovieDtoMapper movieDtoMapper) {
         this.movieService = movieService;
+        this.movieDtoMapper = movieDtoMapper;
     }
 
     @PostMapping
     public MovieResponseDto create(@RequestBody MovieRequestDto movieRequestDto) {
-        return null;
+        Movie movieModel = movieDtoMapper.toModel(movieRequestDto);
+        Movie movieFromDb = movieService.add(movieModel);
+        return movieDtoMapper.parse(movieFromDb);
     }
 
+    @GetMapping
     public List<MovieResponseDto> getAll() {
-        return null;
+        return movieService.getAll()
+                .stream()
+                .map(movieDtoMapper::parse)
+                .collect(Collectors.toList());
     }
 }
