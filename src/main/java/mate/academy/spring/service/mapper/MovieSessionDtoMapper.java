d103b7f7bb47584ac914ed68cf849d56091f767a
docer <vlasovengineer@gmail.com>
2021-08-19 16:32:27 +0300

    create WebConfig create WebAppInitializer fix pom.xml fix field MovieSessionRequestDto fix MovieSessionDtoMapper

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -3,10 +3,20 @@ package mate.academy.spring.service.mapper;
 import mate.academy.spring.dto.MovieSessionRequestDto;
 import mate.academy.spring.dto.MovieSessionResponseDto;
 import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.service.CinemaHallService;
+import mate.academy.spring.service.MovieService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieSessionDtoMapper {
+    private final MovieService movieService;
+    private final CinemaHallService cinemaHallService;
+
+    public MovieSessionDtoMapper(MovieService movieService, CinemaHallService cinemaHallService) {
+        this.movieService = movieService;
+        this.cinemaHallService = cinemaHallService;
+    }
+
     public MovieSessionResponseDto parse(MovieSession movieSession) {
         MovieSessionResponseDto movieSessionResponseDto = new MovieSessionResponseDto();
         movieSessionResponseDto.setId(movieSession.getId());
@@ -18,8 +28,8 @@ public class MovieSessionDtoMapper {
 
     public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
         MovieSession movieSession = new MovieSession();
-        movieSession.setMovie(movieSessionRequestDto.getMovie());
-        movieSession.setCinemaHall(movieSessionRequestDto.getCinemaHall());
+        movieSession.setMovie(movieService.get(movieSessionRequestDto.getMovieId()));
+        movieSession.setCinemaHall(cinemaHallService.get(movieSessionRequestDto.getCinemaHallId()));
         movieSession.setShowTime(movieSessionRequestDto.getShowTime());
         return movieSession;
     }
