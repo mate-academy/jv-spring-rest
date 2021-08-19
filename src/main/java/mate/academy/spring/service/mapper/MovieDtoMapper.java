c3db0e097b80e40254cffba86127ca2720c8b84d
Denis <deniskichan@gmail.com>
2021-08-19 16:14:49 +0300

    implemented controllers and tested them via Postman

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieDtoMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieDtoMapper {
+
+    public MovieResponseDto parse(Movie movie) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        responseDto.setDescription(movie.getDescription());
+        responseDto.setTitle(movie.getTitle());
+        responseDto.setId(movie.getId());
+        return responseDto;
+    }
+
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setDescription(requestDto.getDescription());
+        movie.setTitle(requestDto.getTitle());
+        return movie;
+    }
+}
