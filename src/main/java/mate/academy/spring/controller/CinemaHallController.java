8fe8c64f8726264edf08694564b2d912b0416a00
vKutik <84785564+vKutik@users.noreply.github.com>
2021-08-20 18:02:23 +0300

    add put delete

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cinemahall")
+@RequestMapping("/cinema-hall")
 public class CinemaHallController {
     private final CinemaHallService service;
     private final CinemaHallMapper mapper;
--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -3,20 +3,24 @@ package mate.academy.spring.controller;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
+import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.dto.moviesession.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.moviesession.MovieSessionResponseDto;
 import mate.academy.spring.service.MovieSessionService;
 import mate.academy.spring.service.mapper.MovieSessionMapper;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/session")
+@RequestMapping("/movie-sessions")
 public class MovieSessionController {
     private final MovieSessionService service;
     private final MovieSessionMapper mapper;
@@ -30,6 +34,20 @@ public class MovieSessionController {
     @PostMapping
     public MovieSessionResponseDto add(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
         return mapper.parse(service.add(mapper.parse(movieSessionRequestDto)));
+    }
+
+    @PutMapping("/{id}")
+    public MovieSessionResponseDto add(@PathVariable Long id,
+            @RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+        MovieSession movieSession = mapper.parse(movieSessionRequestDto);
+        movieSession.setId(id);
+        MovieSession updatedMovieSession = service.update(movieSession);
+        return mapper.parse(updatedMovieSession);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
     }
 
     @GetMapping("/")
