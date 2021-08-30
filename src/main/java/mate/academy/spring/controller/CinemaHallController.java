130c620eea3501fa89cec72d1dfbb257a0a290bb
olekskov <oleksandr@kovalenko.cc>
2021-08-30 18:20:32 +0000

    Corr2 - Replaced Impl with Interface in CinemaHallController

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -7,7 +7,7 @@ import mate.academy.spring.model.CinemaHall;
 import mate.academy.spring.model.dto.CinemaHallRequestDto;
 import mate.academy.spring.model.dto.CinemaHallResponseDto;
 import mate.academy.spring.service.CinemaHallService;
-import mate.academy.spring.service.mapper.impl.CinemaHallDtoMapperImpl;
+import mate.academy.spring.service.mapper.CinemaHallDtoMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class CinemaHallController {
     private final CinemaHallService cinemaHallService;
-    private final CinemaHallDtoMapperImpl cinemaHallMapper;
+    private final CinemaHallDtoMapper cinemaHallMapper;
 
     @GetMapping
     public List<CinemaHallResponseDto> getAll() {
