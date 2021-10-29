c439ea7312a5b96b95cecceacf0dfb2327299afe
Evgen Krasnenkov <krasnenkov.evgen@gmail.com>
2021-10-29 12:42:16 +0300

    mr v1 no JSP and dependancies yet added ID in DTO and mapper

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
@@ -3,7 +3,6 @@ package mate.academy.spring.service.mapper;
 import java.time.LocalDateTime;
 import mate.academy.spring.dto.MovieSessionRequestDto;
 import mate.academy.spring.dto.MovieSessionResponseDto;
-import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.MovieService;
