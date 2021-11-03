5ba83710c1815281b105836b1d24b13df57c5e2b
Serhii Orlov <orlov30@gmail.com>
2021-11-03 15:00:06 +0200

    implement controllers for CinemaHall, Movie and MovieSession models in Spring MVC by RESTfull application.

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,40 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import mate.academy.spring.service.MovieService;
+import mate.academy.spring.service.mapper.MovieMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private MovieService movieService;
+    private MovieMapper movieMapper;
+
+    public MovieController(MovieService movieService, MovieMapper movieMapper) {
+        this.movieService = movieService;
+        this.movieMapper = movieMapper;
+    }
+
+    @GetMapping
+    public List<MovieResponseDto> getAll() {
+        return movieService.getAll()
+                .stream()
+                .map(m -> movieMapper.parseToDto(m))
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public MovieResponseDto create(@RequestBody MovieRequestDto requestDto) {
+        return movieMapper.parseToDto(
+                movieService.add(
+                        movieMapper.parseToModel(requestDto)));
+    }
+}
