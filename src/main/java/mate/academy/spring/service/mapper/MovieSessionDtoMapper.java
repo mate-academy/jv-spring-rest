ee8e16951b66966eb9074384572ce1231cffeb16
Solomiia Hrynda <solomiyaaaa@gmail.com>
2021-11-04 12:05:15 +0200

    removed redundant @Autowired annotations

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -5,7 +5,6 @@ import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,6 @@ public class MovieSessionDtoMapper {
     private final MovieService movieService;
     private final CinemaHallService cinemaHallService;
 
-    @Autowired
     public MovieSessionDtoMapper(MovieService movieService, CinemaHallService cinemaHallService) {
         this.movieService = movieService;
         this.cinemaHallService = cinemaHallService;
