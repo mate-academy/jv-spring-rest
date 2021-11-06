aba03be288db53e41a6672c9d1e4c725e497d4b5
Alexandr <netiazhevskyi@gmail.com>
2021-11-06 12:29:21 +0200

    moviesession controller implemented

--- src/main/java/mate/academy/spring/dao/GenericDao.java
+++ src/main/java/mate/academy/spring/dao/GenericDao.java
@@ -2,4 +2,8 @@ package mate.academy.spring.dao;
 
 public interface GenericDao<T> {
     T add(T entity);
+
+    T update(T t);
+
+    void delete(T t);
 }
--- src/main/java/mate/academy/spring/dao/MovieSessionDao.java
+++ src/main/java/mate/academy/spring/dao/MovieSessionDao.java
@@ -9,8 +9,4 @@ public interface MovieSessionDao extends GenericDao<MovieSession> {
     List<MovieSession> findAvailableSessions(Long movieId, LocalDate date);
 
     Optional<MovieSession> get(Long id);
-
-    MovieSession update(MovieSession movieSession);
-
-    void delete(MovieSession movieSession);
 }
