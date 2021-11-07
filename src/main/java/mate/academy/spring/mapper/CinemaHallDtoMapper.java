3c588dabc9664e5668f89f203d425e3bd0880606
Andrii Demchenko <avd998ne@gmail.com>
2021-11-07 13:29:09 +0200

    implemented CinemaHallController

---
+++ src/main/java/mate/academy/spring/mapper/CinemaHallDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.mapper;
+
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.dto.CinemaHallRequestDto;
+import mate.academy.spring.model.dto.CinemaHallResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallDtoMapper {
+    public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto cinemaHallDto = new CinemaHallResponseDto();
+        cinemaHallDto.setId(cinemaHall.getId());
+        cinemaHallDto.setCapacity(cinemaHall.getCapacity());
+        cinemaHallDto.setDescription(cinemaHall.getDescription());
+        return cinemaHallDto;
+    }
+
+    public CinemaHall toModel(CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
+        cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
+        return cinemaHall;
+    }
+}
