73fcc7bec8cae0bff7e174d641ad3814e47ffbb7
Andrii Demchenko <avd998ne@gmail.com>
2021-11-07 16:47:17 +0200

    implemented MovieSessionController

--- src/main/java/mate/academy/spring/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/CinemaHallServiceImpl.java
@@ -21,7 +21,7 @@ public class CinemaHallServiceImpl implements CinemaHallService {
 
     @Override
     public CinemaHall get(Long id) {
-        return cinemaHallDao.get(id).get();
+        return cinemaHallDao.get(id).orElseThrow();
     }
 
     @Override
