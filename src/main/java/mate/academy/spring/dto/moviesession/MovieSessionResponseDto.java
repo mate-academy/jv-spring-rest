02e532b7b9b4ae21bc96dad7d521b6be5d6fe34b
Sinelife <ya.serzhan@gmail.com>
2021-11-10 13:39:01 +0200

    changed showTime in MovieSessionResponseDto to String type

--- src/main/java/mate/academy/spring/dto/moviesession/MovieSessionResponseDto.java
+++ src/main/java/mate/academy/spring/dto/moviesession/MovieSessionResponseDto.java
@@ -1,12 +1,10 @@
 package mate.academy.spring.dto.moviesession;
 
-import java.time.LocalDateTime;
-
 public class MovieSessionResponseDto {
     private Long id;
     private Long movieId;
     private Long cinemaHallId;
-    private LocalDateTime showTime;
+    private String showTime;
 
     public Long getId() {
         return id;
@@ -32,11 +30,11 @@ public class MovieSessionResponseDto {
         this.cinemaHallId = cinemaHallId;
     }
 
-    public LocalDateTime getShowTime() {
+    public String getShowTime() {
         return showTime;
     }
 
-    public void setShowTime(LocalDateTime showTime) {
+    public void setShowTime(String showTime) {
         this.showTime = showTime;
     }
 }
