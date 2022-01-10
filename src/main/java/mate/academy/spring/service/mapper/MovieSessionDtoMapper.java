a84e41b53ef6adbcc03f4ddef027feb379b30d36
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2022-01-10 14:07:53 +0200

    Autowired annotation deleted

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -5,15 +5,14 @@ import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieSessionDtoMapper {
     private final MovieService movieService;
+
     private final CinemaHallService cinemaHallService;
 
-    @Autowired
     public MovieSessionDtoMapper(MovieService movieService, CinemaHallService cinemaHallService) {
         this.movieService = movieService;
         this.cinemaHallService = cinemaHallService;
