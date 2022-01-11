d671ddcf8de89cdc0333e852324c780d7464952c
Pavlo_Martsiniv <imitator2010@gmail.com>
2022-01-11 10:25:56 +0200

    fix errors

--- src/main/java/mate/academy/spring/model/dto/MovieRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/MovieRequestDto.java
@@ -3,10 +3,7 @@ package mate.academy.spring.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Data
 @Builder
--- src/main/java/mate/academy/spring/model/dto/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/MovieSessionRequestDto.java
@@ -4,10 +4,7 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Data
 @Builder
