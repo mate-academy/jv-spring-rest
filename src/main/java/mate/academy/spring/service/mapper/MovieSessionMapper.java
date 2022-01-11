34c2010c083cdf27e2ebde6746bf58cda25e7055
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2022-01-11 17:59:04 +0200

    was implemented controllers for all models

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.dto.MovieSessionRequestDto;
+import mate.academy.spring.model.dto.MovieSessionResponseDto;
+import mate.academy.spring.service.CinemaHallService;
+import mate.academy.spring.service.MovieService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionMapper {
+    private MovieService movieService;
+    private CinemaHallService cinemaHallService;
+
+    public MovieSessionMapper(MovieService movieService, CinemaHallService cinemaHallService) {
+        this.movieService = movieService;
+        this.cinemaHallService = cinemaHallService;
+    }
+
+    public MovieSessionResponseDto parse(MovieSession moviesSession) {
+        MovieSessionResponseDto movieSessionResponseDto = new MovieSessionResponseDto();
+        movieSessionResponseDto.setId(moviesSession.getId());
+        movieSessionResponseDto.setMovieId(moviesSession.getMovie().getId());
+        return movieSessionResponseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(movieService.get(movieSessionRequestDto.getMovieId()));
+        movieSession.setCinemaHall(cinemaHallService.get(movieSessionRequestDto.getCinemaHallId()));
+        movieSession.setShowTime(movieSessionRequestDto.getShowTime());
+        return movieSession;
+    }
+}
