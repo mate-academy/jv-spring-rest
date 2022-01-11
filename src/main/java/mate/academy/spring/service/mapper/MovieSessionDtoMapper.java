347d19f69a004a1133d701bda8ae42130eccdf7c
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2022-01-11 20:19:12 +0200

    after first student review

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -5,7 +5,6 @@ import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,6 @@ public class MovieSessionDtoMapper {
     private CinemaHallService cinemaHallService;
     private MovieService movieService;
 
-    @Autowired
     public MovieSessionDtoMapper(CinemaHallService cinemaHallService, MovieService movieService) {
         this.cinemaHallService = cinemaHallService;
         this.movieService = movieService;
