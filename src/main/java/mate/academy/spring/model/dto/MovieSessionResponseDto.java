16e60c10e4d412c0b5b8e332c012f3839f05f958
Artik <Artik25942@gmail.com>
2022-01-12 15:33:36 +0200

    BuildSucces

---
+++ src/main/java/mate/academy/spring/model/dto/MovieSessionResponseDto.java
@@ -0,0 +1,42 @@
+package mate.academy.spring.model.dto;
+
+import java.time.LocalDateTime;
+
+public class MovieSessionResponseDto {
+    private Long sessionId;
+    private Long movieId;
+    private Long cinemaHallId;
+    private LocalDateTime showTime;
+
+    public Long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(Long sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getCinemaHallId() {
+        return cinemaHallId;
+    }
+
+    public void setCinemaHallId(Long cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
+    }
+}
---
+++ src/main/java/mate/academy/spring/model/mapper/MovieMapper.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.model.mapper;
+
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+    public MovieResponseDto toDto(Movie movie) {
+        MovieResponseDto movieResponseDto = new MovieResponseDto();
+        movieResponseDto.setDescription(movie.getDescription());
+        movieResponseDto.setTitle(movie.getTitle());
+        return movieResponseDto;
+    }
+
+    public Movie toModel(MovieRequestDto movieRequestDto) {
+        Movie movie = new Movie();
+        movie.setDescription(movieRequestDto.getDescription());
+        movie.setTitle(movieRequestDto.getTitle());
+        return movie;
+    }
+}
