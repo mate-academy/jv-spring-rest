970ac0f798d46d5c04119776ddaa6cb37a28a7d1
Volodimir <vova.stepanenko2020@gmail.com>
2022-01-12 16:53:58 +0200

    Del @Autowired from single constructor

--- src/main/java/mate/academy/spring/model/mapper/MovieSessionMapper.java
+++ src/main/java/mate/academy/spring/model/mapper/MovieSessionMapper.java
@@ -5,7 +5,6 @@ import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
