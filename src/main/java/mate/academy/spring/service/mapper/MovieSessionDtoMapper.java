22ecf69e5488861eb2271630637ee21f5ce0f09d
Anatolii Huryk <tolik.mario96@gmail.com>
2022-01-12 13:24:10 +0200

    add annotation

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -5,6 +5,7 @@ import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +13,7 @@ public class MovieSessionDtoMapper {
     private final MovieService movieService;
     private final CinemaHallService cinemaHallService;
 
+    @Autowired
     public MovieSessionDtoMapper(MovieService movieService,
                                  CinemaHallService cinemaHallService) {
         this.movieService = movieService;
