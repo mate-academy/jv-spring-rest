b9dde629933297e01a3ffb3f4fb41186020cbdef
Konstantin Nikiforov <www.kosta.kon@gmail.com>
2022-01-12 17:00:13 +0200

    Unused import was removed.

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -1,6 +1,5 @@
 package mate.academy.spring.service.mapper;
 
-import java.time.LocalDateTime;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
