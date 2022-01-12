901453fdc8c2819766c8845f7a4712e53de8aca2
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2022-01-12 17:13:20 +0200

    fixed

--- src/main/java/mate/academy/spring/model/mapper/MovieSessionMapper.java
+++ src/main/java/mate/academy/spring/model/mapper/MovieSessionMapper.java
@@ -5,7 +5,6 @@ import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,6 @@ public class MovieSessionMapper {
     private MovieService movieService;
     private CinemaHallService cinemaHallService;
 
-    @Autowired
     public MovieSessionMapper(MovieService movieService,
                               CinemaHallService cinemaHallService) {
         this.movieService = movieService;
