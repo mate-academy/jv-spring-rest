cb55b3c92baa4d8d38881d93fbb64e4989e73c09
Alexsis Moskovchuk <alekssiuss@gmail.com>
2022-01-12 09:12:52 +0200

    spring rest hmw

---
+++ src/main/java/mate/academy/spring/model/dto/MovieSessionRequestDto.java
@@ -0,0 +1,42 @@
+package mate.academy.spring.model.dto;
+
+import java.time.LocalDateTime;
+
+public class MovieSessionRequestDto {
+    private LocalDateTime showTime;
+    private Long movieId;
+    private Long cinemaHallId;
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getCinemaHallId() {
+        return cinemaHallId;
+    }
+
+    public void setCinemaHallId(Long cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieSessionRequestDto{"
+                + "showTime=" + showTime
+                + ", movieId=" + movieId
+                + ", cinemaHallId=" + cinemaHallId
+                + '}';
+    }
+}
