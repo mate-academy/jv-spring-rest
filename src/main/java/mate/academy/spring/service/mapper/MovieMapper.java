073f388b59986dacb30083eb10658f1803759221
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-03-23 23:02:28 +0200

    Add Component annotation to MovieMapper

--- src/main/java/mate/academy/spring/service/mapper/MovieMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieMapper.java
@@ -3,7 +3,9 @@ package mate.academy.spring.service.mapper;
 import mate.academy.spring.dto.MovieRequestDto;
 import mate.academy.spring.dto.MovieResponseDto;
 import mate.academy.spring.model.Movie;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MovieMapper {
     public MovieResponseDto toResponseDto(Movie model) {
         MovieResponseDto responseDto = new MovieResponseDto();
