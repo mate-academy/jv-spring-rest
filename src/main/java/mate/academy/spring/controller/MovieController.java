682565acc3419ec8b43b63fa73c4afaa8d443332
Sasha Bachynskyi <allfred12@gmail.com>
2022-03-25 18:14:12 +0200

    implemented MovieController, dto s and mapper

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import mate.academy.spring.service.MovieService;
+import mate.academy.spring.service.mapper.MovieDtoMapper;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private final MovieService movieService;
+    private final MovieDtoMapper movieDtoMapper;
+
+    public MovieController(MovieService movieService, MovieDtoMapper movieDtoMapper) {
+        this.movieService = movieService;
+        this.movieDtoMapper = movieDtoMapper;
+    }
+
+
+    @GetMapping("/")
+    public List<MovieResponseDto> getAllMovies() {
+        return movieService.getAll()
+                .stream()
+                .map(movieDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/")
+    public MovieResponseDto create(@RequestBody MovieRequestDto movieRequestDto) {
+        return movieDtoMapper.parse(movieService.add(movieDtoMapper.toModel(movieRequestDto)));
+    }
+}
