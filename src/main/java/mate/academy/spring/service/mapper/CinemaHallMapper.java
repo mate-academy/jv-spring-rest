6e4247b06357501ef92a0282db709854277cb080
LinkClink <shekaqwe@gmail.com>
2022-03-26 22:47:36 +0200

    mappers

---
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.dto.CinemaHallRequestDto;
+import mate.academy.spring.model.dto.CinemaHallResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallMapper {
+    public CinemaHallResponseDto parse(CinemaHall cinemaHall) {
+        CinemaHallResponseDto cinemaResponseDto = new CinemaHallResponseDto();
+        cinemaResponseDto.setId(cinemaHall.getId());
+        cinemaResponseDto.setDescription(cinemaHall.getDescription());
+        cinemaResponseDto.setCapacity(cinemaHall.getCapacity());
+        return cinemaResponseDto;
+    }
+
+    public CinemaHall toModel(CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
+        cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
+        return cinemaHall;
+    }
+}
---
+++ src/main/java/mate/academy/spring/service/mapper/MovieMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+    public MovieResponseDto parse(Movie movie) {
+        MovieResponseDto movieResponseDto = new MovieResponseDto();
+        movieResponseDto.setId(movie.getId());
+        movieResponseDto.setDescription(movie.getDescription());
+        movieResponseDto.setTitle(movie.getTitle());
+        return movieResponseDto;
+    }
+
+    public Movie toModel(MovieRequestDto movieRequestDto) {
+        Movie movie = new Movie();
+        movie.setDescription(movieRequestDto.getDescription());
+        movie.setTitle(movieRequestDto.getTitle());
+        return movie;
+    }
+}
