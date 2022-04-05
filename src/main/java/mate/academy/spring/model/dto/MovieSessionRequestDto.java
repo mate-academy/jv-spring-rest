c378875fe52da9d51a0429768fd5e0f81fff0d49
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-04-05 18:39:59 +0300

    added pom.xml and all DTOs

---
+++ src/main/java/mate/academy/spring/model/dto/MovieSessionRequestDto.java
@@ -0,0 +1,36 @@
+package mate.academy.spring.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class MovieSessionRequestDto {
+    private Long movieId;
+    private Long cinemaHallId;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime showTime;
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getCinemaHallId() {
+        return cinemaHallId;
+    }
+
+    public void setCinemaHallId(Long cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
+    }
+}
