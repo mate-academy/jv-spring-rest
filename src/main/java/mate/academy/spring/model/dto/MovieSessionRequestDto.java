541b8fa404bbdd54af4bc3574eb3c2dad270fac2
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-04-06 16:46:04 +0300

    added lombok configuration and annotations

--- src/main/java/mate/academy/spring/model/dto/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/MovieSessionRequestDto.java
@@ -2,34 +2,14 @@ package mate.academy.spring.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class MovieSessionRequestDto {
     private Long movieId;
     private Long cinemaHallId;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime showTime;
-
-    public Long getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(Long movieId) {
-        this.movieId = movieId;
-    }
-
-    public Long getCinemaHallId() {
-        return cinemaHallId;
-    }
-
-    public void setCinemaHallId(Long cinemaHallId) {
-        this.cinemaHallId = cinemaHallId;
-    }
-
-    public LocalDateTime getShowTime() {
-        return showTime;
-    }
-
-    public void setShowTime(LocalDateTime showTime) {
-        this.showTime = showTime;
-    }
 }
