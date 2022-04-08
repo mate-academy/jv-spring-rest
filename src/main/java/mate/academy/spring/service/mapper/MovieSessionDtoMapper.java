f8626798c277449c8c07c95dbcbfff16b981b913
Vadym <vadya829@gmail.com>
2022-04-09 01:42:08 +0200

    MovieSessionController POST, GET, PUT, and DELETE methods support was implemented

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -0,0 +1,39 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.dto.MovieSessionRequestDto;
+import mate.academy.spring.model.dto.MovieSessionResponseDto;
+import mate.academy.spring.service.CinemaHallService;
+import mate.academy.spring.service.MovieService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionDtoMapper
+        implements Mapper<MovieSessionRequestDto, MovieSessionResponseDto, MovieSession> {
+    private final MovieService movieService;
+    private final CinemaHallService cinemaHallService;
+
+    public MovieSessionDtoMapper(MovieService movieService, CinemaHallService cinemaHallService) {
+        this.movieService = movieService;
+        this.cinemaHallService = cinemaHallService;
+    }
+
+    @Override
+    public MovieSessionResponseDto toDto(MovieSession movieSession) {
+        MovieSessionResponseDto movieSessionResponseDto = new MovieSessionResponseDto();
+        movieSessionResponseDto.setId(movieSession.getId());
+        movieSessionResponseDto.setMovieId(movieSession.getMovie().getId());
+        movieSessionResponseDto.setCinemaHallId(movieSession.getCinemaHall().getId());
+        movieSessionResponseDto.setShowTime(movieSession.getShowTime());
+        return movieSessionResponseDto;
+    }
+
+    @Override
+    public MovieSession toModel(MovieSessionRequestDto modelRequestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(movieService.get(modelRequestDto.getMovieId()));
+        movieSession.setCinemaHall(cinemaHallService.get(modelRequestDto.getCinemaHallId()));
+        movieSession.setShowTime(modelRequestDto.getShowTime());
+        return movieSession;
+    }
+}
