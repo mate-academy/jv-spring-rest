e85ba8f2353d20fdac67fb7c5eb5f0281ff2a17e
Oleh Mytiai <mytiai.ov@icloud.com>
2022-04-25 23:35:03 +0300

    configure Spring MVC project, added CinemaHallDtoMapper, controller and requestDto, responseDto

---
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.request.CinemaHallRequestDto;
+import mate.academy.spring.dto.response.CinemaHallResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallDtoMapper {
+    public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto responseDto = new CinemaHallResponseDto();
+        responseDto.setId(cinemaHall.getId());
+        responseDto.setCapacity(cinemaHall.getCapacity());
+        responseDto.setDescription(cinemaHall.getDescription());
+        return responseDto;
+    }
+
+    public CinemaHall toModel(CinemaHallRequestDto requestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(requestDto.getCapacity());
+        cinemaHall.setDescription(requestDto.getDescription());
+        return cinemaHall;
+    }
+}
