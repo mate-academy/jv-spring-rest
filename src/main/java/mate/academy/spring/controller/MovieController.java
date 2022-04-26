3a2c45f8842693c40be77bf3254f211a203f4d2b
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-04-26 14:20:06 +0300

    added fixes after mentors review

--- src/main/java/mate/academy/spring/controller/MovieController.java
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -7,7 +7,6 @@ import mate.academy.spring.model.dto.MovieRequestDto;
 import mate.academy.spring.model.dto.MovieResponseDto;
 import mate.academy.spring.service.MovieService;
 import mate.academy.spring.service.mapper.MovieDtoMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +19,6 @@ public class MovieController {
     private final MovieService movieService;
     private final MovieDtoMapper movieDtoMapper;
 
-    @Autowired
     public MovieController(MovieService movieService, MovieDtoMapper movieDtoMapper) {
         this.movieService = movieService;
         this.movieDtoMapper = movieDtoMapper;
