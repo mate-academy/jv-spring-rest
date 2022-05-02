7df5b26d2b4be6a7e60bbabfb6397ca7e9e19c14
Maksym Pokras <mxpokras@gmail.com>
2022-05-02 17:24:19 +0200

    MovieController prepared (dto, mapper) and created

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,41 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.mapper.MovieMapper;
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private final MovieService service;
+    private final MovieMapper mapper;
+
+    public MovieController(MovieService service, MovieMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("")
+    public List<MovieResponseDto> getAll() {
+        return service.getAll().stream()
+                .map(mapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("")
+    public MovieResponseDto add(@RequestBody MovieRequestDto movieRequestDto) {
+        Movie movie = mapper.convert(movieRequestDto);
+        return mapper.parse(service.add(movie));
+    }
+}
