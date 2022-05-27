12509b07d7d1a4e363f331dd54b6234371f40ee6
JadeRowan <rougesoul.m@gmail.com>
2022-05-27 09:16:03 +0300

    Checkstyle fix

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -8,17 +8,15 @@ import mate.academy.spring.model.dto.CinemaHallRequestDto;
 import mate.academy.spring.model.dto.CinemaHallResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/cinema-halls")
-@ResponseBody
 public class CinemaHallController {
     private final CinemaHallService cinemaHallService;
     private final CinemaHallsMapper cinemaHallsMapper;
--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -10,7 +10,6 @@ import mate.academy.spring.model.dto.MovieSessionRespondDto;
 import mate.academy.spring.service.MovieSessionService;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,12 +18,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/movie-sessions")
-@ResponseBody
 public class MovieSessionController {
     private final MovieSessionService movieSessionService;
     private final MovieSessionMapper movieSessionMapper;
