b6c05cde0db9ccc1559e2a21c8dc226671597550
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-06-16 03:00:12 +0300

    Imports refactoring in MovieSessionController

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -1,11 +1,8 @@
 package mate.academy.spring.controller;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
-import mate.academy.spring.model.CinemaHall;
-import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
