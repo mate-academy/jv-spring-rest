5f45292098f0343e24270faf9ae775bb510caf4a
mordast7 <mordast7@gmail.com>
2022-06-16 15:42:00 +0300

    CinemaHallController and MovieController are implemented

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.model.Movie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+    public MovieResponseDto toDto(Movie movie) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        responseDto.setId(movie.getId());
+        responseDto.setDescription(movie.getDescription());
+        responseDto.setTitle(movie.getTitle());
+        return responseDto;
+    }
+
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        return movie;
+    }
+}
