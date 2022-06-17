254eb49a2497321afbdb8f62fca33ea1c33217da
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-06-17 17:25:52 +0300

    Spring rest homework solution has been added

---
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -0,0 +1,41 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.CinemaHallRequestDto;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.mapper.CinemaHallMapper;
+import mate.academy.spring.service.CinemaHallService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cinema-halls")
+public class CinemaHallController {
+    private final CinemaHallService cinemaHallService;
+    private final CinemaHallMapper cinemaHallMapper;
+
+    public CinemaHallController(CinemaHallService cinemaHallService,
+                                CinemaHallMapper cinemaHallMapper) {
+        this.cinemaHallService = cinemaHallService;
+        this.cinemaHallMapper = cinemaHallMapper;
+    }
+
+    @GetMapping
+    public List<CinemaHallResponseDto> getAllCinemaHalls() {
+        return cinemaHallService.getAll()
+                .stream()
+                .map(cinemaHallMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public CinemaHallResponseDto addCinemaHall(
+            @RequestBody CinemaHallRequestDto cinemaHallRequestDto) {
+        return cinemaHallMapper.toDto(cinemaHallService
+                .add(cinemaHallMapper.toModel(cinemaHallRequestDto)));
+    }
+}
---
+++ src/main/java/mate/academy/spring/dto/CinemaHallResponseDto.java
@@ -0,0 +1,41 @@
+package mate.academy.spring.dto;
+
+public class CinemaHallResponseDto {
+
+    private Long id;
+    private int capacity;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "CinemaHall{"
+                + "id=" + id
+                + ", capacity=" + capacity
+                + ", description='" + description + '\''
+                + '}';
+    }
+}
