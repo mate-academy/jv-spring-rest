202c934d74f5beea5242a2552737c343b0eecdfb
Andrey Chumak <chumakandrey7@gmail.com>
2022-07-30 16:10:31 +0300

    Implemented controllers

---
+++ src/main/java/mate/academy/spring/service/dtomapper/MovieDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.dtomapper;
+
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieDtoMapper {
+    public MovieResponseDto toDto(Movie movie) {
+        MovieResponseDto movieResponseDto = new MovieResponseDto();
+        movieResponseDto.setId(movie.getId());
+        movieResponseDto.setDescription(movie.getTitle());
+        movieResponseDto.setDescription(movie.getDescription());
+        return movieResponseDto;
+    }
+
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setDescription(requestDto.getDescription());
+        movie.setTitle(requestDto.getTitle());
+        return movie;
+    }
+}
