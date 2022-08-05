b20af9602fc86f36a428edae547ebcf29f04eda4
Oleksandr Nesteruk <54321777tv@gmail.com>
2022-08-05 10:20:11 +0300

    Dto renamed and fixes added

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -31,7 +31,7 @@ public class MovieSessionController {
 
     @GetMapping("/available")
     public List<MovieSessionResponseDto> getAll(@RequestParam Long movieId,
-                                                @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate date) {
+            @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate date) {
         return movieSessionService.findAvailableSessions(movieId, date).stream()
                 .map(movieSessionMapper::toDto)
                 .collect(Collectors.toList());
