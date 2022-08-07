1b52a24c3d0a1c22294467867ac2225193d52292
Karina Tanasiichuk <k.tanasiichuk18@gmail.com>
2022-08-07 19:24:46 +0300

    movie, movie session and cinema hall controllers were created

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
@@ -0,0 +1,27 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.MovieSessionRequestDto;
+import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.model.MovieSession;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionMapper {
+
+    public MovieSessionResponseDto toDto(MovieSession movieSession) {
+        MovieSessionResponseDto movieSessionResponseDto = new MovieSessionResponseDto();
+        movieSessionResponseDto.setId(movieSession.getId());
+        movieSessionResponseDto.setMovie(movieSession.getMovie());
+        movieSessionResponseDto.setCinemaHall(movieSession.getCinemaHall());
+        movieSessionResponseDto.setShowTime(movieSession.getShowTime());
+        return movieSessionResponseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(movieSessionRequestDto.getMovie());
+        movieSession.setCinemaHall(movieSessionRequestDto.getCinemaHall());
+        movieSession.setShowTime(movieSession.getShowTime());
+        return movieSession;
+    }
+}
