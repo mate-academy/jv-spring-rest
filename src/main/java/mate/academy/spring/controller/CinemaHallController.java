91bd2df706a59cf44dda56b3b2e949caa1d17000
Mykola Yakymiv <mykola8426@gmail.com>
2022-08-08 12:00:13 +0300

    fixes

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -7,7 +7,12 @@ import mate.academy.spring.dto.CinemaHallResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.mapper.CinemaHallDtoMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cinema-halls")
