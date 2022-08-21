89b102e18361a71c87193d107da116d8e4070094
drapail <mv.dibrov@gmail.com>
2022-08-21 19:46:25 +0300

    MovieSessionController implemented

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -1,8 +1,45 @@
 package mate.academy.spring.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.MovieSessionRequestDto;
+import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.service.MovieSessionService;
+import mate.academy.spring.service.mapper.MovieSessionDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MovieSessionController {
+    private final MovieSessionService movieSessionService;
+    private final MovieSessionDtoMapper movieSessionDtoMapper;
 
+    @Autowired
+    public MovieSessionController(MovieSessionService movieSessionService,
+                                  MovieSessionDtoMapper movieSessionDtoMapper) {
+        this.movieSessionService = movieSessionService;
+        this.movieSessionDtoMapper = movieSessionDtoMapper;
+    }
+
+    @PostMapping("/movie-sessions")
+    @ResponseBody
+    private MovieSessionResponseDto create(@RequestParam
+                                           @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate date,
+                                           MovieSessionRequestDto movieSessionRequestDto) {
+        movieSessionRequestDto.setShowTime(date);
+        return movieSessionDtoMapper.parse(movieSessionService
+                .add(movieSessionDtoMapper.toModel(movieSessionRequestDto)));
+    }
+
+    @GetMapping("/movie-sessions/available")
+    @ResponseBody
+    private List<MovieSessionResponseDto> getAvailableSessions(@RequestParam Long movieId,
+                                                                @RequestParam LocalDate date) {
+        return movieSessionService.findAvailableSessions(movieId, date).stream()
+                .map(movieSessionDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
 }
---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -0,0 +1,38 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.MovieSessionRequestDto;
+import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.service.CinemaHallService;
+import mate.academy.spring.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MovieSessionDtoMapper {
+    private final MovieService movieService;
+    private final CinemaHallService cinemaHallService;
+
+    @Autowired
+    public MovieSessionDtoMapper(MovieService movieService, CinemaHallService cinemaHallService) {
+        this.movieService = movieService;
+        this.cinemaHallService = cinemaHallService;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setShowTime(movieSessionRequestDto.getShowTime());
+        movieSession.setMovie(movieService.get(movieSessionRequestDto.getMovieId()));
+        movieSession.setCinemaHall(cinemaHallService.get(movieSessionRequestDto.getCinemaHallId()));
+        return movieSession;
+    }
+
+    public MovieSessionResponseDto parse(MovieSession movieSession) {
+        MovieSessionResponseDto movieSessionResponseDto = new MovieSessionResponseDto();
+        movieSessionResponseDto.setMovieId(movieSession.getMovie().getId());
+        movieSessionResponseDto.setCinemaHallId(movieSession.getCinemaHall().getId());
+        movieSessionResponseDto.setShowTime(movieSession.getShowTime());
+        return movieSessionResponseDto;
+    }
+}
