44b98f85351dfed0556eaf5f36924af5f50a3767
Oleksii Mospan <alexey.mospan@gmail.com>
2022-09-13 15:13:21 +0300

    Added controller for CinemaHall

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -1,4 +1,55 @@
 package mate.academy.spring.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.CinemaHallRequestDto;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.service.CinemaHallService;
+import mate.academy.spring.service.mapper.CinemaHallDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cinema-halls")
 public class CinemaHallController {
+    private final CinemaHallService cinemaHallService;
+    private final CinemaHallDtoMapper cinemaHallDtoMapper;
+
+    @Autowired
+    public CinemaHallController(CinemaHallService cinemaHallService,
+                                CinemaHallDtoMapper cinemaHallDtoMapper) {
+        this.cinemaHallService = cinemaHallService;
+        this.cinemaHallDtoMapper = cinemaHallDtoMapper;
+    }
+
+    @PostMapping
+    public void add(@RequestBody CinemaHallRequestDto cinemaHallDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(cinemaHallDto.getCapacity());
+        cinemaHall.setDescription(cinemaHallDto.getDescription());
+        cinemaHallService.add(cinemaHall);
+    }
+
+    @GetMapping("/{id}")
+    public CinemaHallResponseDto get(@PathVariable Long id) {
+        CinemaHall cinemaHall = cinemaHallService.get(id);
+        CinemaHallResponseDto responseDto = new CinemaHallResponseDto();
+        responseDto.setId(id);
+        responseDto.setCapacity(cinemaHall.getCapacity());
+        responseDto.setDescription(cinemaHall.getDescription());
+        return responseDto;
+    }
+
+    @GetMapping("")
+    public List<CinemaHallResponseDto> getAll() {
+        return cinemaHallService.getAll().stream()
+                .map(cinemaHallDtoMapper::toDto)
+                .collect(Collectors.toList());
+    }
 }
