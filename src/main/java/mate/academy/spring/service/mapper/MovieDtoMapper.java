c27d7795973ecd650ce7128d8506d84355705955
Oleksii Mospan <alexey.mospan@gmail.com>
2022-09-13 03:32:55 +0300

    Added mappers for CinemaHall, Movie, MovieSession

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.model.Movie;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieDtoMapper {
+    public MovieResponseDto toDto(Movie movie) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        responseDto.setId(movie.getId());
+        responseDto.setTitle(movie.getTitle());
+        responseDto.setDescription(movie.getDescription());
+        return responseDto;
+    }
+
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        return movie;
+    }
+}
