e96006fdcfe5ae793c8b2e51d87fd0755b98c7a5
Oleksii Mospan <alexey.mospan@gmail.com>
2022-09-13 15:13:42 +0300

    Added controller for Movie

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,54 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.service.MovieService;
+import mate.academy.spring.service.mapper.MovieDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private final MovieService movieService;
+    private final MovieDtoMapper movieDtoMapper;
+
+    @Autowired
+    public MovieController(MovieService movieService, MovieDtoMapper movieDtoMapper) {
+        this.movieService = movieService;
+        this.movieDtoMapper = movieDtoMapper;
+    }
+
+    @GetMapping("/{id}")
+    public MovieResponseDto get(@PathVariable Long id) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        Movie movie = movieService.get(id);
+        responseDto.setId(id);
+        responseDto.setTitle(movie.getTitle());
+        responseDto.setDescription((movie.getDescription()));
+        return responseDto;
+    }
+
+    @PostMapping
+    public void add(@RequestBody MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        movieService.add(movie);
+    }
+
+    @GetMapping("")
+    public List<MovieResponseDto> getAll() {
+        return movieService.getAll().stream()
+                .map(movieDtoMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
