1e294f717dadf34397e8536dd7275fa97a883760
Benatti <ssunica0@gmail.com>
2022-09-15 16:56:53 +0300

    completed task

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieDtoMapper {
+    public MovieResponseDto parse(Movie movie) {
+        MovieResponseDto movieResponseDto = new MovieResponseDto();
+        movieResponseDto.setId(movie.getId());
+        movieResponseDto.setTitle(movie.getTitle());
+        movieResponseDto.setDescription(movie.getDescription());
+        return movieResponseDto;
+    }
+
+    public Movie toModel(MovieRequestDto request) {
+        Movie movie = new Movie();
+        movie.setTitle(request.getTitle());
+        movie.setDescription(request.getDescription());
+        return movie;
+    }
+}
