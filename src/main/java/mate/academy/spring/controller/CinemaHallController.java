60f06843f2e7cdeb85413af111f008c4a27a9883
Oleksii Akishev <akishev.oleksii@gmail.com>
2022-10-02 19:34:04 +0300

    MovieController was implemented

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -1,5 +1,9 @@
 package mate.academy.spring.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.dto.CinemaHallRequestDto;
+import mate.academy.spring.model.dto.CinemaHallResponseDto;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.mapper.CinemaHallDtoMapper;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-import java.util.stream.Collectors;
-import mate.academy.spring.model.dto.CinemaHallRequestDto;
-import mate.academy.spring.model.dto.CinemaHallResponseDto;
 
 @RestController
 @RequestMapping("/cinema-halls")
@@ -26,14 +26,14 @@ public class CinemaHallController {
 
     @PostMapping
     public CinemaHallResponseDto create(@RequestBody CinemaHallRequestDto cinemaHall) {
-         return cinemaHallDtoMapper.parse(cinemaHallService
+         return cinemaHallDtoMapper.toDto(cinemaHallService
                  .add(cinemaHallDtoMapper.toModel(cinemaHall)));
     }
 
     @GetMapping
     public List<CinemaHallResponseDto> getAll() {
         return cinemaHallService.getAll().stream()
-                .map(cinemaHallDtoMapper::parse)
+                .map(cinemaHallDtoMapper::toDto)
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/mate/academy/spring/service/mapper/CinemaHallDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallDtoMapper.java
@@ -1,13 +1,13 @@
 package mate.academy.spring.service.mapper;
 
-import org.springframework.stereotype.Component;
 import mate.academy.spring.model.CinemaHall;
 import mate.academy.spring.model.dto.CinemaHallRequestDto;
 import mate.academy.spring.model.dto.CinemaHallResponseDto;
+import org.springframework.stereotype.Component;
 
 @Component
 public class CinemaHallDtoMapper {
-    public CinemaHallResponseDto parse(CinemaHall cinemaHall) {
+    public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
         CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
         cinemaHallResponseDto.setId(cinemaHall.getId());
         cinemaHallResponseDto.setDescription(cinemaHall.getDescription());
