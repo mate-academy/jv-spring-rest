044350525b283b15be42591971bdb2b6d33627d3
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-10-07 08:05:29 +0300

    resolved

---
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.CinemaHallRequestDto;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallMapper {
+    public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto responseDto = new CinemaHallResponseDto();
+        responseDto.setId(cinemaHall.getId());
+        responseDto.setDescription(cinemaHall.getDescription());
+        responseDto.setCapacity(cinemaHall.getCapacity());
+        return responseDto;
+    }
+
+    public CinemaHall toModel(CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
+        cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
+        return cinemaHall;
+    }
+}
