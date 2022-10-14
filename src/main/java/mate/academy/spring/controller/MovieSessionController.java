dd957de30f87a5f37331ff09de89de80f801a082
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-10-14 14:08:05 +0300

    add controlers, config files, dto and mappers

---
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -0,0 +1,44 @@
+package mate.academy.spring.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.MovieSessionRequestDto;
+import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.mappers.MovieSessionMapper;
+import mate.academy.spring.service.MovieSessionService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie_session")
+public class MovieSessionController {
+    private MovieSessionService movieSessionService;
+    private MovieSessionMapper movieSessionMapper;
+
+    public MovieSessionController(MovieSessionService movieSessionService,
+                                  MovieSessionMapper movieSessionMapper) {
+        this.movieSessionService = movieSessionService;
+        this.movieSessionMapper = movieSessionMapper;
+    }
+
+    @GetMapping("/available")
+    public List<MovieSessionResponseDto> getAvailableSessions(
+            @RequestParam Long id,
+            @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy")LocalDate localDate) {
+        return movieSessionService.findAvailableSessions(id, localDate).stream()
+                .map(movieSessionMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public MovieSessionResponseDto create(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+        return movieSessionMapper.toDto(movieSessionService
+                .add(movieSessionMapper.toModel(movieSessionRequestDto)));
+    }
+}
---
+++ src/main/java/mate/academy/spring/mappers/MovieMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.mappers;
+
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.model.Movie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+    public MovieResponseDto toDto(Movie movie) {
+        MovieResponseDto movieResponseDto =
+                new MovieResponseDto();
+        movieResponseDto.setId(movie.getId());
+        movieResponseDto.setTitle(movieResponseDto.getTitle());
+        movieResponseDto.setDescription(movieResponseDto.getDescription());
+        return movieResponseDto;
+    }
+
+    public Movie toModel(MovieRequestDto movieRequestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(movieRequestDto.getTitle());
+        movie.setDescription(movieRequestDto.getDescription());
+        return movie;
+    }
+}
