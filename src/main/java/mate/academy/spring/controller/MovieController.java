0697347b184d5ba245d2853bbc68650b1e987930
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-15 20:31:02 +0200

    implement MovieMapper

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,12 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.service.MovieService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movies")
+public class MovieController {
+    private MovieService movieService;
+    private
+}
---
+++ src/main/java/mate/academy/spring/mapper/MovieMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.mapper;
+
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.model.Movie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        return movie;
+    }
+
+    public MovieResponseDto toDto(Movie movie) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        responseDto.setId(movie.getId());
+        responseDto.setTitle(movie.getTitle());
+        responseDto.setDescription(movie.getDescription());
+        return responseDto;
+    }
+}
