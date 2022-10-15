fac410561f70b5ec9ebb05c3046895aa40bc4dca
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-15 18:45:57 +0200

    add dto for CinemaHall

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -1,4 +1,25 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.service.CinemaHallService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cinema_halls")
 public class CinemaHallController {
+    private CinemaHallService cinemaHallService;
+
+    @Autowired
+    public CinemaHallController(CinemaHallService cinemaHallService) {
+        this.cinemaHallService = cinemaHallService;
+    }
+
+    @GetMapping
+    public CinemaHall getAll(CinemaHall cinemaHall) {
+        return
+    }
+
 }
