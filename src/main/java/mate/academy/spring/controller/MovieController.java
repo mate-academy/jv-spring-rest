afe69ead93fd56a2464c4375c9a739e6645e4be3
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-15 20:48:06 +0200

    implement MovieController methods

--- src/main/java/mate/academy/spring/controller/MovieController.java
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -1,12 +1,39 @@
 package mate.academy.spring.controller;
 
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.mapper.MovieMapper;
 import mate.academy.spring.service.MovieService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
 @RequestMapping("/movies")
 public class MovieController {
     private MovieService movieService;
-    private
+    private MovieMapper movieMapper;
+
+    public MovieController(MovieService movieService,
+                           MovieMapper movieMapper) {
+        this.movieService = movieService;
+        this.movieMapper = movieMapper;
+    }
+
+    @PostMapping
+    public MovieResponseDto create(@RequestBody MovieRequestDto requestDto) {
+        return movieMapper.toDto(movieService.add(movieMapper.toModel(requestDto)));
+    }
+
+    @GetMapping
+    public List<MovieResponseDto> getAll() {
+        return movieService.getAll().stream()
+                .map(movieMapper::toDto)
+                .collect(Collectors.toList());
+    }
 }
