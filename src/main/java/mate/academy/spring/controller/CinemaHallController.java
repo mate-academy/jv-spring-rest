ca714b094361ef1d7144ff4e12131694359d8068
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-15 19:24:48 +0200

    fix db.properties and implement getAll()

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -1,6 +1,9 @@
 package mate.academy.spring.controller;
 
-import mate.academy.spring.model.CinemaHall;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.mapper.CinemaHallMapper;
 import mate.academy.spring.service.CinemaHallService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cinema_halls")
 public class CinemaHallController {
     private CinemaHallService cinemaHallService;
+    private CinemaHallMapper cinemaHallMapper;
 
     @Autowired
-    public CinemaHallController(CinemaHallService cinemaHallService) {
+    public CinemaHallController(CinemaHallService cinemaHallService,
+                                CinemaHallMapper cinemaHallMapper) {
         this.cinemaHallService = cinemaHallService;
+        this.cinemaHallMapper = cinemaHallMapper;
     }
 
     @GetMapping
-    public CinemaHall getAll(CinemaHall cinemaHall) {
-        return
+    public List<CinemaHallResponseDto> getAll() {
+        return cinemaHallService.getAll()
+                .stream()
+                .map(cinemaHallMapper::toDto)
+                .collect(Collectors.toList());
     }
 
 }
