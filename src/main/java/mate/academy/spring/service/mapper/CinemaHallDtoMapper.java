4622830f96f7226d7bbc0b1f0ae80b8c717d0d13
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-18 20:20:20 +0300

    CinemaHallController with getAll, CinemaHallResponseDto,CinemaHallDtoMapper with parse were added

---
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallDtoMapper.java
@@ -0,0 +1,16 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.dto.CinemaHallResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallDtoMapper {
+    public CinemaHallResponseDto parse(CinemaHall cinemaHall) {
+        CinemaHallResponseDto responseDto = new CinemaHallResponseDto();
+        responseDto.setId(cinemaHall.getId());
+        responseDto.setDescription(cinemaHall.getDescription());
+        responseDto.setCapacity(cinemaHall.getCapacity());
+        return responseDto;
+    }
+}
