307c81b6adf3f5d9f82c50577420759d164e8331
Danylo Frolov <frolovdanilik@gmail.com>
2022-10-19 15:59:32 +0300

    solution

---
+++ src/main/java/mate/academy/spring/model/dto/response/MovieResponseDto.java
@@ -0,0 +1,31 @@
+package mate.academy.spring.model.dto.response;
+
+public class MovieResponseDto {
+    private String title;
+    private String description;
+    private Long id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
