393ca81190c70630c413d02bb554797a1a737289
LubusUzh <lubus.uzh@gmail.com>
2022-10-19 14:18:46 +0300

    Add: methods update and delete for the MovieSession in the dao and service layers and complete implementation in the MovieSessionController class

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -1,15 +1,22 @@
 package mate.academy.spring.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 import mate.academy.spring.dto.MovieSessionRequestDto;
 import mate.academy.spring.dto.MovieSessionResponseDto;
 import mate.academy.spring.mapper.MovieSessionMapper;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.service.MovieSessionService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,5 +36,24 @@ public class MovieSessionController {
     }
 
     @GetMapping("/available")
-    public List<MovieSessionResponseDto> getAllAvailable()
+    public List<MovieSessionResponseDto> findAvailableSessions(@RequestParam Long movieId,
+                                                               @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate date) {
+        return service.findAvailableSessions(movieId, date).stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping("/{id}")
+    public MovieSessionResponseDto update(@PathVariable Long id,
+                                          @RequestBody MovieSessionRequestDto requestDto) {
+        MovieSession movieSession = mapper.toModel(requestDto);
+        movieSession.setId(id);
+        return mapper.toDto(service.update(movieSession));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public MovieSessionResponseDto delete(@PathVariable Long id) {
+        return mapper.toDto(service.delete(service.get(id)));
+    }
 }
