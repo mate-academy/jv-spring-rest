077835c990578bd5f0c90cc9b09d21bd9c0513fb
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-19 14:44:57 +0300

    MovieSessionDtoMapper, MovieSessionRequestDto, MovieSessionResponseDto were added

---
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -0,0 +1,10 @@
+package mate.academy.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie-sessions")
+public class MovieSessionController {
+
+}
