f28e8fc2e61e06696909a476be33c6f628be5d7b
Volodymyr Tsupryk <lordvolodua@gmail.com>
2022-10-19 16:10:37 +0300

    done homework

---
+++ src/main/java/mate/academy/spring/mapper/MovieMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.mapper;
+
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        return movie;
+    }
+
+    public MovieResponseDto toDto(Movie movie) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        responseDto.setId(movie.getId());
+        responseDto.setTitle(movie.getTitle());
+        responseDto.setDescription(movie.getDescription());
+        return responseDto;
+    }
+}
