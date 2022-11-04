db60335a77698afff7f31ea5335a602bbc768436
Igor Petrichenko <i.petrichenko@gmail.com>
2022-11-04 09:41:06 +0200

    HW spring rest

---
+++ src/main/java/mate/academy/spring/dto/response/MovieResponseDto.java
@@ -0,0 +1,41 @@
+package mate.academy.spring.dto.response;
+
+public class MovieResponseDto {
+    private Long id;
+    private String title;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("MovieResponseDto{");
+        sb.append("id=").append(id);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/mate/academy/spring/mapper/MovieMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.mapper;
+
+import mate.academy.spring.dto.request.MovieRequestDto;
+import mate.academy.spring.dto.response.MovieResponseDto;
+import mate.academy.spring.model.Movie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+
+    public MovieResponseDto parse(Movie movie) {
+        MovieResponseDto movieResponseDto = new MovieResponseDto();
+        movieResponseDto.setId(movie.getId());
+        movieResponseDto.setTitle(movie.getTitle());
+        movieResponseDto.setDescription(movie.getDescription());
+        return movieResponseDto;
+    }
+
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        return movie;
+    }
+}
