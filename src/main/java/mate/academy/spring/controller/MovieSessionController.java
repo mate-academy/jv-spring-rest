4eb14ff32e028925f698c5560a85fd08b03c37ee
Nazarii Pertsak <pertsak98@gmail.com>
2022-12-09 16:00:39 +0200

    changes after mentor review

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/movie_sessions")
+@RequestMapping("/movie-sessions")
 public class MovieSessionController {
     private MovieSessionService movieSessionService;
     private MovieSessionDtoMapper mapper;
