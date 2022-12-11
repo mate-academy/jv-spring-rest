347cbdfa30187c01b42a73b3147ed2500cd898e1
Anatolii Bespalko <tolyan9901@gmail.com>
2022-12-12 01:25:00 +0200

    spring rest

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.request.MovieRequestDto;
+import mate.academy.spring.dto.response.MovieResponseDto;
+import mate.academy.spring.model.Movie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieDtoMapper {
+    public Movie toModel(MovieRequestDto movieRequestDto) {
+        Movie movie = new Movie();
+        movie.setDescription(movieRequestDto.getDescription());
+        movie.setTitle(movieRequestDto.getTitle());
+        return movie;
+    }
+
+    public MovieResponseDto toDto(Movie movie) {
+        MovieResponseDto movieResponseDto = new MovieResponseDto();
+        movieResponseDto.setId(movie.getId());
+        movieResponseDto.setDescription(movie.getDescription());
+        movieResponseDto.setTitle(movie.getTitle());
+        return movieResponseDto;
+    }
+}
