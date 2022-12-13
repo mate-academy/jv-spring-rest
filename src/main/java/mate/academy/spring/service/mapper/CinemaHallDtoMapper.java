30a4e71d5d24848dee2d2a308888c6e3a7b41724
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-12-13 15:08:06 +0200

    realize spring rest

---
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.CinemaHallRequestDto;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallDtoMapper {
+    public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto responseDto = new CinemaHallResponseDto();
+        responseDto.setId(cinemaHall.getId());
+        responseDto.setDescription(cinemaHall.getDescription());
+        responseDto.setCapacity(cinemaHall.getCapacity());
+        return responseDto;
+    }
+
+    public CinemaHall toModel(CinemaHallRequestDto requestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setDescription(requestDto.getDescription());
+        cinemaHall.setCapacity(requestDto.getCapacity());
+        return cinemaHall;
+    }
+}
