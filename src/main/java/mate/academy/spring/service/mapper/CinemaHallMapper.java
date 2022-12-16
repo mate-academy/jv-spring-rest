36b2de67fd4b78f05b4313784d122b9842444192
Veronika Tretiak <nikatretiak9@gmail.com>
2022-12-16 16:19:25 +0100

    Implemented task

---
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.request.CinemaHallRequestDto;
+import mate.academy.spring.dto.response.CinemaHallResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallMapper {
+    public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
+        cinemaHallResponseDto.setId(cinemaHall.getId());
+        cinemaHallResponseDto.setDescription(cinemaHall.getDescription());
+        cinemaHallResponseDto.setCapacity(cinemaHall.getCapacity());
+        return cinemaHallResponseDto;
+    }
+
+    public CinemaHall toModel(CinemaHallRequestDto requestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setDescription(requestDto.getDescription());
+        cinemaHall.setCapacity(requestDto.getCapacity());
+        return cinemaHall;
+    }
+}
