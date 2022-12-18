9526d1fb1e92f8a0750c07de02f0ef73737853a0
NSay0 <krvipa@gmail.com>
2022-12-18 19:13:09 +0200

    CinemaHallDtoMapper corrected

--- src/main/java/mate/academy/spring/service/mapper/CinemaHallDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallDtoMapper.java
@@ -22,6 +22,6 @@ public class CinemaHallDtoMapper
         CinemaHall cinemaHall = new CinemaHall();
         cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
         cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
-        return null;
+        return cinemaHall;
     }
 }
