4da76fd83db3e022411d8ee7638eebb468bf5350
Mark <markrybnikov2000@gmail.com>
2022-12-18 19:11:28 +0300

    Created all controllers, all dto, dto mapper

---
+++ src/main/java/mate/academy/spring/dto/MovieSessionResponseDto.java
@@ -0,0 +1,42 @@
+package mate.academy.spring.dto;
+
+import java.time.LocalDateTime;
+
+public class MovieSessionResponseDto {
+    private Long id;
+    private Long movieId;
+    private Long cinemaHallId;
+    private LocalDateTime localDateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getCinemaHallId() {
+        return cinemaHallId;
+    }
+
+    public void setCinemaHallId(Long cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+}
