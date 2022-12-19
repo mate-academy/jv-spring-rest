335e1f21c8c8e20aa4664187bf2fd6dc3b006aaa
Andrii Matveev <matveev.tesla@gmail.com>
2022-12-19 18:00:55 +0200

    homework has been done

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -0,0 +1,37 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.MovieSessionRequestDto;
+import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.service.CinemaHallService;
+import mate.academy.spring.service.MovieService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionDtoMapper {
+    private MovieService movieService;
+    private CinemaHallService cinemaHallService;
+
+    public MovieSessionDtoMapper(MovieService movieService,
+                                 CinemaHallService cinemaHallService) {
+        this.movieService = movieService;
+        this.cinemaHallService = cinemaHallService;
+    }
+
+    public MovieSessionResponseDto parse(MovieSession movieSession) {
+        MovieSessionResponseDto responseDto = new MovieSessionResponseDto();
+        responseDto.setMovieId(movieSession.getMovie().getId());
+        responseDto.setCinemaHallId(movieSession.getCinemaHall().getId());
+        responseDto.setShowTime(movieSession.getShowTime());
+        responseDto.setId(movieSession.getId());
+        return responseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto requestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(movieService.get(requestDto.getMovieId()));
+        movieSession.setCinemaHall(cinemaHallService.get(requestDto.getCinemaHallId()));
+        movieSession.setShowTime(requestDto.getShowTime());
+        return movieSession;
+    }
+}
