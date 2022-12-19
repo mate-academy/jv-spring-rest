667d247fdfb9544d2337a2fd77551d30336496a8
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-12-19 21:20:01 +0200

    removed redundant import

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -1,7 +1,6 @@
 package mate.academy.spring.controller;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import mate.academy.spring.model.MovieSession;
