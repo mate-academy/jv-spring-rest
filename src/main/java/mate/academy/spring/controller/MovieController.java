0e1f5eb741c8ab94a0617ef868a665f50e2ef2c2
Alexander <aleksandr_pogreb@ukr.net>
2022-12-26 15:59:32 +0200

    solution

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,38 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.mapper.MovieMapper;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import mate.academy.spring.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private final MovieService movieService;
+    private final MovieMapper movieMapper;
+
+    public MovieController(MovieService movieService, MovieMapper movieMapper) {
+        this.movieService = movieService;
+        this.movieMapper = movieMapper;
+    }
+
+    @GetMapping
+    public List<MovieResponseDto> getAll() {
+        return movieService.getAll().stream()
+                .map(movieMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping
+    public MovieResponseDto create(@RequestBody MovieRequestDto movieRequestDto) {
+        return movieMapper.toDto(movieService.add(
+                movieMapper.toModel(movieRequestDto)));
+    }
+}
