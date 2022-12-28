b46c20adbe4dc8143ec06c4684bd44a568a8fd01
zipic <zpc23041988@gmail.com>
2022-12-28 20:16:18 +0200

    spring_rest_complete

---
+++ src/main/java/mate/academy/spring/dto/MovieSessionRequestDto.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.dto;
+
+import java.time.LocalDateTime;
+
+public class MovieSessionRequestDto {
+
+    private Long movieId;
+    private Long cinemaHallId;
+    private LocalDateTime localDateTime;
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getCinemaHallId() {
+        return cinemaHallId;
+    }
+
+    public void setCinemaHallId(Long cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+}
