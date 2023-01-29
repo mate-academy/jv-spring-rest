18d051f25ba2e57d417891b08d397cfcb16952cb
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-29 17:57:11 +0200

    add WebAppInitializer and WebConfig

---
+++ src/main/java/mate/academy/spring/config/WebConfig.java
@@ -0,0 +1,9 @@
+package mate.academy.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "mate.academy.spring")
+public class WebConfig {
+}
