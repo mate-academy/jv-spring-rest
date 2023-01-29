54d33f4fc40bc0ddec98f97fd3a85f72bcdfca14
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-01-29 18:31:48 +0200

    Add Movie Controller

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,43 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.mapper.MovieMapper;
+import mate.academy.spring.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MovieController {
+    private final MovieService movieService;
+    private final MovieMapper movieMapper;
+
+    @Autowired
+    public MovieController(MovieService movieService,
+                           MovieMapper movieMapper) {
+        this.movieService = movieService;
+        this.movieMapper = movieMapper;
+    }
+
+    @PostMapping("/movies")
+    public MovieResponseDto add(@RequestBody MovieRequestDto movie) {
+        return movieMapper.toDto(movieService.add(movieMapper.toModel(movie)));
+    }
+
+    @ResponseBody
+    @GetMapping("/movies")
+    public List<MovieResponseDto> getAll(){
+        return movieService
+                .getAll()
+                .stream()
+                .map(movieMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
+
