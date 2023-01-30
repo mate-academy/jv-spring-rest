44f5f31d316149e895c6696c63042c4be0407e26
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-01-30 13:17:54 +0200

    Change annotation

--- src/main/java/mate/academy/spring/mapper/CinemaHallMapper.java
+++ src/main/java/mate/academy/spring/mapper/CinemaHallMapper.java
@@ -3,9 +3,9 @@ package mate.academy.spring.mapper;
 import mate.academy.spring.dto.CinemaHallRequestDto;
 import mate.academy.spring.dto.CinemaHallResponseDto;
 import mate.academy.spring.model.CinemaHall;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class CinemaHallMapper {
     public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
         CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
