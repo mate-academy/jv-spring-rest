29c246ae568af1ac2827e595abd973dfa1f00225
Lev <levberezov01@gmail.com>
2023-02-03 19:32:53 +0200

    added fixes

--- src/main/java/mate/academy/spring/controller/MovieSessionController.java
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -3,7 +3,6 @@ package mate.academy.spring.controller;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.dto.MovieSessionRequest;
 import mate.academy.spring.model.dto.MovieSessionResponse;
