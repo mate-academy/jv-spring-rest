989c4eb42c515298ee2f799e716b93401f282b1e
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2023-02-27 18:19:02 +0200

    spring rest 2d commit bugs fixed

--- src/main/java/mate/academy/spring/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/MovieSessionDaoImpl.java
@@ -82,19 +82,19 @@ public class MovieSessionDaoImpl extends AbstractDao<MovieSession> implements Mo
     }
 
     @Override
-    public void delete(Long id) {
+    public void delete(MovieSession movieSession) {
         Session session = null;
         Transaction transaction = null;
         try {
             session = sessionFactory.openSession();
             transaction = session.beginTransaction();
-            session.delete(session.get(MovieSession.class, id));
+            session.delete(movieSession);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't delete a movie session by id: " + id, e);
+            throw new DataProcessingException("Can't delete a movie session: " + movieSession, e);
         } finally {
             if (session != null) {
                 session.close();
