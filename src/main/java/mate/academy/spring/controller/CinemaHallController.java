716d0b82d476e3cd08c1539f1277cbc7d09813b9
Polina Lysytska <nikolavna100500@gmail.com>
2023-02-28 13:18:32 +0200

    tasks were implemented

---
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -0,0 +1,37 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.dto.CinemaHallRequestDto;
+import mate.academy.spring.model.dto.CinemaHallResponseDto;
+import mate.academy.spring.service.CinemaHallService;
+import mate.academy.spring.service.mapper.CinemaHallMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cinema-halls")
+public class CinemaHallController {
+    private final CinemaHallService service;
+    private final CinemaHallMapper mapper;
+
+    public CinemaHallController(CinemaHallService service, CinemaHallMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @PostMapping
+    public CinemaHallResponseDto create(@RequestBody CinemaHallRequestDto requestDto) {
+        return mapper.toDto(service.add(mapper.toModel(requestDto)));
+    }
+
+    @GetMapping
+    public List<CinemaHallResponseDto> getAll() {
+        return service.getAll().stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
