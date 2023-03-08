d865383724056f234dff4fc3b08ada8f1fb8e4a6
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-03-08 14:49:47 +0200

    feat: jv-spring-rest #comment was implemented rest endpoints for Movie, Cinema Hall and MovieSession

--- src/main/java/mate/academy/spring/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/MovieSessionServiceImpl.java
@@ -2,6 +2,7 @@ package mate.academy.spring.service.impl;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import mate.academy.spring.dao.MovieSessionDao;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.service.MovieSessionService;
@@ -28,5 +29,19 @@ public class MovieSessionServiceImpl implements MovieSessionService {
     @Override
     public MovieSession add(MovieSession session) {
         return sessionDao.add(session);
+    }
+
+    @Override
+    public MovieSession update(MovieSession session) {
+        return sessionDao.update(session);
+    }
+
+    @Override
+    public int delete(Long sessionId) {
+        Optional<MovieSession> movieSession = sessionDao.get(sessionId);
+        if (movieSession.isEmpty()) {
+            return 0;
+        }
+        return sessionDao.delete(movieSession.get());
     }
 }
---
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallDtoMapper.java
@@ -0,0 +1,26 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.CinemaHallRequestDto;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallDtoMapper {
+    public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
+        cinemaHallResponseDto.setCapacity(cinemaHall.getCapacity());
+        cinemaHallResponseDto.setId(cinemaHall.getId());
+        cinemaHallResponseDto.setDescription(cinemaHall.getDescription());
+
+        return cinemaHallResponseDto;
+    }
+
+    public CinemaHall fromDto(CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
+        cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
+
+        return cinemaHall;
+    }
+}
