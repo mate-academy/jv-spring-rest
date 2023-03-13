03766692812f9127eb7d64410721ce3668fd6414
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2023-03-13 15:12:21 +0200

    Created mappers

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+
+public class MovieMapper {
+    public MovieResponseDto parse(Movie movie) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        responseDto.setId(movie.getId());
+        responseDto.setTitle(movie.getDescription());
+        responseDto.setDescription(movie.getDescription());
+        return responseDto;
+    }
+
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        return movie;
+    }
+}
