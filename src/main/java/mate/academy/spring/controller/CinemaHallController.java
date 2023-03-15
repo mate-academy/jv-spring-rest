10355bc3e6a20f194583dbb4c21ab1ffbe9e2dee
Diana <dlomei283@gmail.com>
2023-03-15 14:08:41 +0200

    changes

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cinemahalls")
+@RequestMapping("/cinema-halls")
 public class CinemaHallController {
     private final CinemaHallDtoMapper cinemaHallDtoMapper;
     private final CinemaHallService cinemaHallService;
