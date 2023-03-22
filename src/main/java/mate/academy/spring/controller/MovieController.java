0bf6a3b0e91352549c741b9481d50a418352f039
Andrii Varlanovych <andrew_exp@ukr.net>
2023-03-22 10:22:58 +0200

    was solved

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,42 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.movie.MovieRequestDto;
+import mate.academy.spring.model.dto.movie.MovieResponseDto;
+import mate.academy.spring.service.MovieService;
+import mate.academy.spring.service.mapper.MovieMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private final MovieService movieService;
+    private final MovieMapper movieMapper;
+
+    @Autowired
+    public MovieController(MovieService movieService, MovieMapper movieMapper) {
+        this.movieService = movieService;
+        this.movieMapper = movieMapper;
+    }
+
+    @PostMapping
+    public MovieResponseDto creat(@RequestBody MovieRequestDto movieRequestDto) {
+        Movie movieFromDB = movieService.add(movieMapper.toModel(movieRequestDto));
+        return movieMapper.toDto(movieFromDB);
+    }
+
+    @GetMapping("/")
+    public List<MovieResponseDto> getAllMovies() {
+        return movieService.getAll()
+                .stream()
+                .map(movieMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
