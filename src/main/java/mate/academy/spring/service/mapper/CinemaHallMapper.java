4e5ba0d276a162a765c950662312faf5c524e6c5
Kate <katerynachi5@gmail.com>
2023-03-30 19:57:32 +0200

    Implement CinemaHall, Movie and MovieSession controllers

---
+++ src/main/java/mate/academy/spring/service/mapper/CinemaHallMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.dto.CinemaHallRequestDto;
+import mate.academy.spring.model.dto.CinemaHallResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallMapper {
+    public CinemaHallResponseDto toDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto responseDto = new CinemaHallResponseDto();
+        responseDto.setId(cinemaHall.getId());
+        responseDto.setDescription(cinemaHall.getDescription());
+        responseDto.setCapacity(cinemaHall.getCapacity());
+        return responseDto;
+    }
+
+    public CinemaHall toModel(CinemaHallRequestDto requestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setDescription(requestDto.getDescription());
+        cinemaHall.setCapacity(requestDto.getCapacity());
+        return cinemaHall;
+    }
+}
