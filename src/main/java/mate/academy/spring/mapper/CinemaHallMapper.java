7c4f24c07339656fc9b6b298b333ea087f2328e8
Olexii Melnik <alexmelnikk@gmail.com>
2023-03-31 14:36:39 +0300

    second commit

--- src/main/java/mate/academy/spring/mapper/CinemaHallMapper.java
+++ src/main/java/mate/academy/spring/mapper/CinemaHallMapper.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CinemaHallMapper {
-
     public CinemaHallResponseDto parse(CinemaHall cinemaHall) {
         CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
         cinemaHallResponseDto.setId(cinemaHall.getId());
--- src/main/java/mate/academy/spring/mapper/MovieSessionMapper.java
+++ src/main/java/mate/academy/spring/mapper/MovieSessionMapper.java
@@ -2,22 +2,13 @@ package mate.academy.spring.mapper;
 
 import mate.academy.spring.dto.MovieSessionRequestDto;
 import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.MovieSession;
-import mate.academy.spring.service.CinemaHallService;
-import mate.academy.spring.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieSessionMapper {
-    private final MovieService movieService;
-    private final CinemaHallService cinemaHallService;
-
-    @Autowired
-    public MovieSessionMapper(MovieService movieService, CinemaHallService cinemaHallService) {
-        this.movieService = movieService;
-        this.cinemaHallService = cinemaHallService;
-    }
 
     public MovieSessionResponseDto parse(MovieSession movieSession) {
         MovieSessionResponseDto movieSessionResponseDto =
@@ -32,8 +23,8 @@ public class MovieSessionMapper {
     public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
         MovieSession movieSession = new MovieSession();
         movieSession.setShowTime(movieSessionRequestDto.getShowTime());
-        movieSession.setMovie(movieService.get(movieSessionRequestDto.getMovieId()));
-        movieSession.setCinemaHall(cinemaHallService.get(movieSessionRequestDto.getCinemaHallId()));
+        movieSession.setMovie(new Movie(movieSessionRequestDto.getMovieId()));
+        movieSession.setCinemaHall(new CinemaHall(movieSessionRequestDto.getCinemaHallId()));
         return movieSession;
     }
 }
