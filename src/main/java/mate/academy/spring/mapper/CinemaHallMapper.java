98b457f75d74b4917e7f72034bae1278e816614a
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-04-08 18:20:18 +0200

    Task spring-rest solved

---
+++ src/main/java/mate/academy/spring/mapper/CinemaHallMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.mapper;
+
+import mate.academy.spring.dto.CinemaHallRequestDto;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallMapper {
+    public CinemaHallResponseDto parse(CinemaHall cinemaHall) {
+        CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
+        cinemaHallResponseDto.setId(cinemaHall.getId());
+        cinemaHallResponseDto.setCapacity(cinemaHall.getCapacity());
+        cinemaHallResponseDto.setDescription(cinemaHall.getDescription());
+        return cinemaHallResponseDto;
+    }
+
+    public CinemaHall toModel(CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
+        cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
+        return cinemaHall;
+    }
+}
---
+++ src/main/java/mate/academy/spring/mapper/MovieSessionMapper.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.mapper;
+
+import mate.academy.spring.dto.MovieSessionRequestDto;
+import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.MovieSession;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionMapper {
+    public MovieSessionResponseDto parse(MovieSession movieSession) {
+        MovieSessionResponseDto movieSessionResponseDto =
+                new MovieSessionResponseDto();
+        movieSessionResponseDto.setId(movieSession.getId());
+        movieSessionResponseDto.setShowTime(movieSession.getShowTime());
+        movieSessionResponseDto.setMovieId(movieSession.getMovie().getId());
+        movieSessionResponseDto.setCinemaHallId(movieSession.getCinemaHall().getId());
+        return movieSessionResponseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setShowTime(movieSessionRequestDto.getShowTime());
+        movieSession.setMovie(new Movie(movieSessionRequestDto.getMovieId()));
+        movieSession.setCinemaHall(new CinemaHall(movieSessionRequestDto.getCinemaHallId()));
+        return movieSession;
+    }
+}
