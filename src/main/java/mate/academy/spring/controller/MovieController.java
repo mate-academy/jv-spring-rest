174bb2849a3e0f904f2138530d036f15cf225cc7
Vasyl Piznak <piznak3145@gmail.com>
2023-04-12 20:48:12 +0300

    Commit

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import mate.academy.spring.service.MovieService;
+import mate.academy.spring.service.mapper.MovieMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @Autowired
+    private MovieMapper movieMapper;
+
+    @PostMapping
+    public MovieResponseDto add(@RequestBody MovieRequestDto movieRequestDto) {
+        return movieMapper.toDto(movieService.add(movieMapper.toModel(movieRequestDto)));
+    }
+
+    @GetMapping
+    public List<MovieResponseDto> getAll() {
+        return movieService.getAll().stream()
+               .map(m -> movieMapper.toDto(m)).collect(Collectors.toList());
+    }
+}
