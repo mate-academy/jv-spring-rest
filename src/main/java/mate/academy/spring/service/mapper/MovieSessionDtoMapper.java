4a5357d884a3997deeac6c68d8b9448271e42694
Vitalina Volkivska <113260060+Vitalina-V@users.noreply.github.com>
2023-04-19 20:25:47 +0300

    my spring-rest hm is done

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.dto.MovieSessionRequestDto;
+import mate.academy.spring.model.dto.MovieSessionResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionDtoMapper {
+
+    public MovieSessionResponseDto toDto(MovieSession movieSession) {
+        MovieSessionResponseDto movieSessionResponseDto = new MovieSessionResponseDto();
+        movieSessionResponseDto.setId(movieSession.getId());
+        movieSessionResponseDto.setCinemaHall(movieSession.getCinemaHall());
+        movieSessionResponseDto.setShowTime(movieSession.getShowTime());
+        return movieSessionResponseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto requestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setCinemaHall(requestDto.getCinemaHall());
+        movieSession.setShowTime(requestDto.getShowTime());
+        return movieSession;
+    }
+}
