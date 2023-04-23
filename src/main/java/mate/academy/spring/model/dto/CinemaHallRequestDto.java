ba8503855341d0e794670d015039c40f362a4910
Skrudj <ssinilov@gmail.com>
2023-04-23 17:18:49 +0300

    create initial extension structure

---
+++ src/main/java/mate/academy/spring/model/dto/CinemaHallRequestDto.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.model.dto;
+
+public class CinemaHallRequestDto {
+    private int capacity;
+    private String description;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "CinemaHallRequestDto{"
+                + "capacity=" + capacity + '\''
+                + ", description='" + description + '\''
+                + '}';
+    }
+}
