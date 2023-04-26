fd0daab8b56d8f4d3d624ff6551f2e989e6493bb
Ilko Good <ilko.engineering@gmail.com>
2023-04-26 13:04:17 +0300

    Review fix

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
@@ -5,20 +5,11 @@ import mate.academy.spring.dto.MovieSessionResponseDto;
 import mate.academy.spring.model.CinemaHall;
 import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.MovieSession;
-import mate.academy.spring.service.CinemaHallService;
-import mate.academy.spring.service.MovieService;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieSessionMapper {
-    private final MovieService movieService;
-    private final CinemaHallService cinemaHallService;
-
-    public MovieSessionMapper(MovieService movieService, CinemaHallService cinemaHallService) {
-        this.movieService = movieService;
-        this.cinemaHallService = cinemaHallService;
-    }
 
     public MovieSessionResponseDto getDto(@NonNull MovieSession movieSession) {
         MovieSessionResponseDto responseDto = new MovieSessionResponseDto();
