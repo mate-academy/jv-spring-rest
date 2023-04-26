f6c59d1a094bd206b6b033578aa4bf5b53877008
Ilko Good <ilko.engineering@gmail.com>
2023-04-26 12:30:12 +0300

    Switched to Hibernate magic in MovieSessionMapper

--- src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
@@ -2,6 +2,8 @@ package mate.academy.spring.service.mapper;
 
 import mate.academy.spring.dto.MovieSessionRequestDto;
 import mate.academy.spring.dto.MovieSessionResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.service.CinemaHallService;
 import mate.academy.spring.service.MovieService;
@@ -29,8 +31,12 @@ public class MovieSessionMapper {
 
     public MovieSession getModel(@NonNull MovieSessionRequestDto requestDto) {
         MovieSession movieSession = new MovieSession();
-        movieSession.setMovie(movieService.get(requestDto.getMovieId()));
-        movieSession.setCinemaHall(cinemaHallService.get(requestDto.getCinemaHallId()));
+        Movie movie = new Movie();
+        movie.setId(requestDto.getMovieId());
+        movieSession.setMovie(movie);
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setId(requestDto.getCinemaHallId());
+        movieSession.setCinemaHall(cinemaHall);
         movieSession.setShowTime(requestDto.getShowTime());
         return movieSession;
     }
