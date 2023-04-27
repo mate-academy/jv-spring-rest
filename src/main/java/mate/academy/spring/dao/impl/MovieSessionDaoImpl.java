6ebe0292477328b8462b39117bb0e3379af05c42
De1eF <jatfun58@gmail.com>
2023-04-27 16:43:15 +0200

    DAO message fix

--- src/main/java/mate/academy/spring/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/MovieSessionDaoImpl.java
@@ -89,7 +89,7 @@ public class MovieSessionDaoImpl extends AbstractDao<MovieSession> implements Mo
                             + "WHERE ms.id = :id", MovieSession.class).setParameter("id", id)
                     .uniqueResultOptional();
         } catch (Exception e) {
-            throw new DataProcessingException("Can't get a movie session by id: " + id, e);
+            throw new DataProcessingException("Can't remove movie session by id: " + id, e);
         }
     }
 }
