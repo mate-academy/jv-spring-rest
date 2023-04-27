e95e67770662c28ceef08ce5a43399251cad00b7
Sushko Roman <formatesus@gmail.com>
2023-04-27 22:04:03 +0300

    created Controllers

---
+++ src/main/java/mate/academy/spring/controller/MovieController.java
@@ -0,0 +1,48 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import mate.academy.spring.service.MovieService;
+import mate.academy.spring.service.mapper.MovieMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private final MovieMapper movieMapper;
+    private final MovieService movieService;
+
+    public MovieController(MovieMapper movieMapper,
+                           MovieService movieService) {
+        this.movieMapper = movieMapper;
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<MovieResponseDto> getAll() {
+        return movieService.getAll()
+                .stream()
+                .map(movieMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public MovieResponseDto create(MovieRequestDto requestDto) {
+        return movieMapper.toDto(
+                movieService.add(movieMapper.toModel(requestDto)));
+    }
+
+    @GetMapping("/t")
+    public String inject() {
+        Movie movie = new Movie();
+        movie.setTitle("Terminator");
+        movieService.add(movie);
+        return "Ok";
+    }
+}
---
+++ src/main/java/mate/academy/spring/controller/MovieSessionController.java
@@ -0,0 +1,56 @@
+package mate.academy.spring.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.dto.MovieSessionRequestDto;
+import mate.academy.spring.model.dto.MovieSessionResponseDto;
+import mate.academy.spring.service.MovieSessionService;
+import mate.academy.spring.service.mapper.MovieSessionMapper;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie-sessions")
+public class MovieSessionController {
+    private final MovieSessionMapper sessionMapper;
+    private final MovieSessionService sessionService;
+
+    public MovieSessionController(MovieSessionMapper sessionMapper,
+                                  MovieSessionService sessionService) {
+        this.sessionMapper = sessionMapper;
+        this.sessionService = sessionService;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        sessionService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public MovieSessionResponseDto update(@RequestBody MovieSessionRequestDto requestDto,
+                                          @PathVariable Long id) {
+        MovieSession model = sessionMapper.toModel(requestDto);
+        model.setId(id);
+        MovieSession updated = sessionService.update(model);
+        return sessionMapper.toDto(updated);
+    }
+
+    @GetMapping("/available")
+    public List<MovieSessionResponseDto> findAvailableSession(
+            @RequestParam Long movieId, @RequestParam @DateTimeFormat(
+                    pattern = "dd.MM.yyyy")LocalDate localDate) {
+        return sessionService.findAvailableSessions(movieId, localDate)
+                .stream()
+                .map(sessionMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
