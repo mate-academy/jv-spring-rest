4bef0106b599cc48f9d7504f7345feb848f772a0
Nadiia Huryk <nadia11huryk@gmail.com>
2023-04-28 00:15:21 +0300

    Spring REST

---
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -0,0 +1,42 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.CinemaHallRequestDto;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.mapper.CinemaHallDtoMapper;
+import mate.academy.spring.service.CinemaHallService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cinema-halls")
+public class CinemaHallController {
+    private CinemaHallDtoMapper cinemaHallDtoMapper;
+    private CinemaHallService cinemaHallService;
+
+    @Autowired
+    public CinemaHallController(CinemaHallDtoMapper cinemaHallDtoMapper,
+                                CinemaHallService cinemaHallService) {
+        this.cinemaHallDtoMapper = cinemaHallDtoMapper;
+        this.cinemaHallService = cinemaHallService;
+
+    }
+
+    @GetMapping
+    public List<CinemaHallResponseDto> getAll() {
+        return cinemaHallService.getAll().stream()
+                .map(cinemaHallDtoMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public CinemaHallResponseDto create(@RequestBody CinemaHallRequestDto cinemaHallRequestDto) {
+        return cinemaHallDtoMapper.toDto(cinemaHallService.add(cinemaHallDtoMapper
+                .toModel(cinemaHallRequestDto)));
+    }
+}
