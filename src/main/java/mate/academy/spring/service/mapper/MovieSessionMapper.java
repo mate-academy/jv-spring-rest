edbca0fcabcd0ca71202614a186e2550e3e1f0b3
Sushko Roman <formatesus@gmail.com>
2023-04-27 22:03:44 +0300

    created Mappers

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionMapper.java
@@ -0,0 +1,29 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.dto.MovieSessionRequestDto;
+import mate.academy.spring.model.dto.MovieSessionResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionMapper {
+    public MovieSessionResponseDto toDto(MovieSession movieSession) {
+        MovieSessionResponseDto responseDto = new MovieSessionResponseDto();
+        responseDto.setId(movieSession.getId());
+        responseDto.setMovieId(movieSession.getMovie().getId());
+        responseDto.setCinemaHallId(movieSession.getCinemaHall().getId());
+        responseDto.setShowTime(movieSession.getShowTime());
+        return responseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto requestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(new Movie(requestDto.getMovieId()));
+        movieSession.setCinemaHall(new CinemaHall(requestDto.getCinemaHallId()));
+        movieSession.setShowTime(requestDto.getShowTime());
+        return movieSession;
+    }
+
+}
