2c87d44f573fff61f9c243541a4bf4887d512ee3
De1eF <jatfun58@gmail.com>
2023-04-28 08:50:18 +0200

    Fixed

--- src/main/java/mate/academy/spring/model/dto/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/CinemaHallRequestDto.java
@@ -1,17 +1,8 @@
 package mate.academy.spring.model.dto;
 
 public class CinemaHallRequestDto {
-    private Long id;
     private int capacity;
     private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public int getCapacity() {
         return capacity;
