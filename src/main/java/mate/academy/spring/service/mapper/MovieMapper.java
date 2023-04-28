a99ca2a56dd8569ad0fe2dc4ba4a03a5fcb3e4fa
Nickolay Voloshchuk <124300295+NickolayVoloshchuk@users.noreply.github.com>
2023-04-28 14:19:15 +0300

    HomeWork Java/Spring REST

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.dto.MovieRequestDto;
+import mate.academy.spring.model.dto.MovieResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+    public Movie parse(MovieRequestDto dto) {
+        Movie movie = new Movie();
+        movie.setTitle(dto.getTitle());
+        movie.setDescription(dto.getDescription());
+        return movie;
+    }
+
+    public MovieResponseDto parse(Movie movie) {
+        MovieResponseDto dto = new MovieResponseDto();
+        dto.setId(movie.getId());
+        dto.setTitle(movie.getTitle());
+        dto.setDescription(movie.getDescription());
+        return dto;
+    }
+}
