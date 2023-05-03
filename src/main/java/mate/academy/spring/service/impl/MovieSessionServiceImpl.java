a7ee4118f882fcdb675028336802b4929c1123db
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-05-03 12:11:53 +0300

    did task: "Spring REST"

--- src/main/java/mate/academy/spring/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/MovieSessionServiceImpl.java
@@ -2,15 +2,18 @@ package mate.academy.spring.service.impl;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.academy.spring.dao.MovieSessionDao;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.service.MovieSessionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieSessionServiceImpl implements MovieSessionService {
     private final MovieSessionDao sessionDao;
 
+    @Autowired
     public MovieSessionServiceImpl(MovieSessionDao sessionDao) {
         this.sessionDao = sessionDao;
     }
@@ -28,5 +31,17 @@ public class MovieSessionServiceImpl implements MovieSessionService {
     @Override
     public MovieSession add(MovieSession session) {
         return sessionDao.add(session);
+    }
+
+    @Override
+    public MovieSession update(MovieSession movieSession) {
+        return sessionDao.update(movieSession).orElseThrow(() ->
+                new NoSuchElementException("The movie session is absent in DB")
+        );
+    }
+
+    @Override
+    public void delete(Long id) {
+        sessionDao.delete(sessionDao.get(id).orElseThrow());
     }
 }
---
+++ src/main/java/mate/academy/spring/service/mapper/MovieDtoMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.MovieRequestDto;
+import mate.academy.spring.dto.MovieResponseDto;
+import mate.academy.spring.model.Movie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieDtoMapper {
+
+    public MovieResponseDto parse(Movie movie) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        responseDto.setId(movie.getId());
+        responseDto.setDescription(movie.getDescription());
+        responseDto.setTitle(movie.getTitle());
+        return responseDto;
+    }
+
+    public Movie toModel(MovieRequestDto requestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        return movie;
+    }
+}
