9c1c58c440f1de911d51cc9e23f6e44bca991db3
Yevhenii Kilovyi <118058456+YevheniiKilovyi@users.noreply.github.com>
2023-05-10 13:10:36 +0300

    Configured Spring Web, added all required dependencies, added WebConfig and WebAppInitiliazer, implemented DTO mappers for CinemaHall , Movie and MovieSession, added update() and delete() method on DAO and service layer of MovieSession, created all needed DTO models and implemented 3 controllers: MovieSessionController, MovieController and CinemaHall controller

--- src/main/java/mate/academy/spring/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/MovieSessionServiceImpl.java
@@ -2,6 +2,7 @@ package mate.academy.spring.service.impl;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.academy.spring.dao.MovieSessionDao;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.service.MovieSessionService;
@@ -28,5 +29,17 @@ public class MovieSessionServiceImpl implements MovieSessionService {
     @Override
     public MovieSession add(MovieSession session) {
         return sessionDao.add(session);
+    }
+
+    @Override
+    public MovieSession update(MovieSession movieSession) {
+        return sessionDao.update(movieSession)
+                .orElseThrow(() ->
+                        new NoSuchElementException("Can't update movie session: " + movieSession));
+    }
+
+    @Override
+    public void delete(MovieSession movieSession) {
+        sessionDao.delete(movieSession);
     }
 }
