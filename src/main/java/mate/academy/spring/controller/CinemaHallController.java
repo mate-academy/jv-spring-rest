015a0394ce06c53ac32448969f2ab7fad54196ea
antonsablev <live.incntr@gmail.com>
2023-06-05 19:48:10 +0200

    fix GetMapping Annotation

--- src/main/java/mate/academy/spring/controller/CinemaHallController.java
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -26,7 +26,7 @@ public class CinemaHallController {
         this.cinemaHallMapper = cinemaHallMapper;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public List<CinemaHallResponseDto> getAll() {
         return cinemaHallService.getAll().stream()
                 .map(cinemaHallMapper::parse)
