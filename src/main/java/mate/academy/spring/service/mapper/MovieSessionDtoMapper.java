137cf4237f7b669d7f8b112c81e01a807be6464c
Andrii Shevchuk <zovten156@gmail.com>
2023-06-09 11:47:58 +0300

    was created controllers

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.dto.MovieSessionRequestDto;
+import mate.academy.spring.model.dto.MovieSessionResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionDtoMapper {
+    public MovieSessionResponseDto toDto(MovieSession movieSession) {
+        MovieSessionResponseDto movieSessionResponseDto = new MovieSessionResponseDto();
+        movieSessionResponseDto.setMovieId(movieSession.getMovie().getId());
+        movieSessionResponseDto.setCinemaHallId(movieSession.getCinemaHall().getId());
+        movieSessionResponseDto.setShowTime(movieSession.getShowTime());
+        return movieSessionResponseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setCinemaHall(movieSessionRequestDto.getCinemaHall());
+        movieSession.setMovie(movieSessionRequestDto.getMovie());
+        movieSession.setShowTime(movieSessionRequestDto.getShowTime());
+        return movieSession;
+    }
+}
