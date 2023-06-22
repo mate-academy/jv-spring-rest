1c37408e4f702ef296fa5eac2d990be24375f508
Melnychuk <melnychukmyhailo@gmail.com>
2023-06-22 17:12:00 +0300

    ResponseDto and RequestDto were created for MovieSession, Movie, CinemaHall models.

---
+++ src/main/java/mate/academy/spring/model/dto/MovieSessionResponseDto.java
@@ -0,0 +1,33 @@
+package mate.academy.spring.model.dto;
+
+import java.time.LocalDateTime;
+
+public class MovieSessionResponseDto {
+    private Long id;
+    private String movieTitle;
+    private LocalDateTime showTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
+    }
+}
