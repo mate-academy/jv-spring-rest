b1ced5045b1c71395e7c5a6fd3552c6b815f45ef
Vladyslav Krutyus <kijdron@gmail.com>
2023-07-07 12:45:04 +0300

    fixed problem with MovieSessionMapper implementation

--- src/main/java/mate/academy/spring/service/mapper/impl/MovieSessionMapperImpl.java
+++ src/main/java/mate/academy/spring/service/mapper/impl/MovieSessionMapperImpl.java
@@ -1,23 +1,15 @@
 package mate.academy.spring.service.mapper.impl;
 
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
-import mate.academy.spring.service.CinemaHallService;
-import mate.academy.spring.service.MovieService;
 import mate.academy.spring.service.mapper.MovieSessionMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieSessionMapperImpl implements MovieSessionMapper {
-    private final MovieService movieService;
-    private final CinemaHallService cinemaHallService;
-
-    public MovieSessionMapperImpl(MovieService movieService, CinemaHallService cinemaHallService) {
-        this.movieService = movieService;
-        this.cinemaHallService = cinemaHallService;
-    }
-
     @Override
     public MovieSessionResponseDto toDto(MovieSession movieSession) {
         MovieSessionResponseDto dto = new MovieSessionResponseDto();
@@ -31,8 +23,8 @@ public class MovieSessionMapperImpl implements MovieSessionMapper {
     @Override
     public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
         MovieSession movieSession = new MovieSession();
-        movieSession.setMovie(movieService.get(movieSessionRequestDto.getMovieId()));
-        movieSession.setCinemaHall(cinemaHallService.get(movieSessionRequestDto.getCinemaHallId()));
+        movieSession.setMovie(new Movie(movieSessionRequestDto.getMovieId()));
+        movieSession.setCinemaHall(new CinemaHall(movieSessionRequestDto.getCinemaHallId()));
         movieSession.setShowTime(movieSessionRequestDto.getShowTime());
         return movieSession;
     }
