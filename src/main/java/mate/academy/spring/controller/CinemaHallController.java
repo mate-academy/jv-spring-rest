4b1d60c66d61a27fa993ac6b2c3d144b74c35d65
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-07-17 21:43:23 +0300

    cinemaHallController doesn't receive json

---
+++ src/main/java/mate/academy/spring/controller/CinemaHallController.java
@@ -0,0 +1,39 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.dto.CinemaHallRequestDto;
+import mate.academy.spring.dto.CinemaHallResponseDto;
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.service.CinemaHallService;
+import mate.academy.spring.service.mapper.CinemaHallDtoMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/cinema_halls")
+public class CinemaHallController {
+    private final CinemaHallService cinemaHallService;
+    private final CinemaHallDtoMapper cinemaHallDtoMapper;
+
+    public CinemaHallController(CinemaHallService cinemaHallService, CinemaHallDtoMapper cinemaHallDtoMapper) {
+        this.cinemaHallService = cinemaHallService;
+        this.cinemaHallDtoMapper = cinemaHallDtoMapper;
+    }
+
+    @PostMapping
+    public CinemaHallResponseDto addCinemaHall(@RequestBody CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall modelFromDto = cinemaHallDtoMapper.toModel(cinemaHallRequestDto);
+        CinemaHall cinemaHall = cinemaHallService.add(modelFromDto);
+        return cinemaHallDtoMapper.parse(cinemaHall);
+
+
+    }
+
+    @GetMapping
+    public List<CinemaHallResponseDto> getAllCinemaHalls() {
+        return cinemaHallService.getAll().stream()
+                .map(cinemaHallDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+}
