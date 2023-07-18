f261f06d2dba067c671df1180f2b1c48c551c2b6
vgladkyi <vasyl.gladky@gmail.com>
2023-07-18 21:42:22 +0300

    Implemented all required controllers/dto's

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -0,0 +1,26 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.dto.MovieSessionRequestDto;
+import mate.academy.spring.model.dto.MovieSessionResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionDtoMapper {
+    public MovieSessionResponseDto parse(MovieSession movieSession) {
+        MovieSessionResponseDto movieSessionResponseDto = new MovieSessionResponseDto();
+        movieSessionResponseDto.setId(movieSession.getId());
+        movieSessionResponseDto.setMovie(movieSession.getMovie());
+        movieSessionResponseDto.setCinemaHall(movieSession.getCinemaHall());
+        movieSessionResponseDto.setShowTime(movieSession.getShowTime());
+        return movieSessionResponseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(movieSessionRequestDto.getMovie());
+        movieSession.setCinemaHall(movieSessionRequestDto.getCinemaHall());
+        movieSession.setShowTime(movieSessionRequestDto.getShowTime());
+        return movieSession;
+    }
+}
