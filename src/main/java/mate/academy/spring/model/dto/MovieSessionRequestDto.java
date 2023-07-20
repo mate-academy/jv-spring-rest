54c0663ee7044894a4fb95cadf374dd307730fb8
vgladkyi <vasyl.gladky@gmail.com>
2023-07-20 15:08:01 +0300

    MovieSessionRequestDto/MovieSessionResponseDto and mapper were fixed

--- src/main/java/mate/academy/spring/model/dto/MovieSessionRequestDto.java
+++ src/main/java/mate/academy/spring/model/dto/MovieSessionRequestDto.java
@@ -1,7 +1,12 @@
 package mate.academy.spring.model.dto;
 
+import java.time.LocalDateTime;
+
 public class MovieSessionRequestDto {
     private Long id;
+    private Long movieId;
+    private Long cinemaHallId;
+    private LocalDateTime showTime;
 
     public MovieSessionRequestDto() {
     }
@@ -12,5 +17,29 @@ public class MovieSessionRequestDto {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getCinemaHallId() {
+        return cinemaHallId;
+    }
+
+    public void setCinemaHallId(Long cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
     }
 }
