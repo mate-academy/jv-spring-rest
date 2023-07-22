a4cdbee7cacc934c81d847d16890e4574afaeb72
Trach Vika <vikaa@Oksanas-MacBook-Pro.local>
2023-07-22 18:19:45 +0300

    hw-spring-rest

---
+++ src/main/java/mate/academy/spring/service/mapper/MovieSessionDtoMapper.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.Movie;
+import mate.academy.spring.model.MovieSession;
+import mate.academy.spring.model.dto.MovieSessionRequestDto;
+import mate.academy.spring.model.dto.MovieSessionResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSessionDtoMapper {
+    public MovieSessionResponseDto parse(MovieSession movieSession) {
+        MovieSessionResponseDto responseDto = new MovieSessionResponseDto();
+        responseDto.setId(movieSession.getId());
+        responseDto.setMovieId(movieSession.getMovie().getId());
+        responseDto.setCinemaHallId(movieSession.getCinemaHall().getId());
+        responseDto.setShowTime(movieSession.getShowTime());
+        return responseDto;
+    }
+
+    public MovieSession toModel(MovieSessionRequestDto requestDto) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(new Movie(requestDto.getMovieId()));
+        movieSession.setCinemaHall(new CinemaHall(requestDto.getCinemaHallId()));
+        movieSession.setShowTime(requestDto.getShowTime());
+        return movieSession;
+    }
+}
