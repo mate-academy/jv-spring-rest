404281cf72a5ef06f8adc465ba3059192efa2805
Kristina Shpilchyna <kriss.mva@gmail.com>
2023-07-26 13:17:38 +0200

    changed MovieSession toModel using new Movie(id) and new CinemaHall(id)

--- src/main/java/mate/academy/spring/service/mappers/MovieSessionDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mappers/MovieSessionDtoMapper.java
@@ -1,23 +1,14 @@
 package mate.academy.spring.service.mappers;
 
+import mate.academy.spring.model.CinemaHall;
+import mate.academy.spring.model.Movie;
 import mate.academy.spring.model.MovieSession;
 import mate.academy.spring.model.dto.MovieSessionRequestDto;
 import mate.academy.spring.model.dto.MovieSessionResponseDto;
-import mate.academy.spring.service.CinemaHallService;
-import mate.academy.spring.service.MovieService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieSessionDtoMapper {
-    private final MovieService movieService;
-    private final CinemaHallService cinemaHallService;
-
-    public MovieSessionDtoMapper(MovieService movieService,
-                                 CinemaHallService cinemaHallService) {
-        this.movieService = movieService;
-        this.cinemaHallService = cinemaHallService;
-    }
-
     public MovieSessionResponseDto toDto(MovieSession movieSession) {
         MovieSessionResponseDto movieSessionDto = new MovieSessionResponseDto();
         movieSessionDto.setId(movieSession.getId());
@@ -30,8 +21,8 @@ public class MovieSessionDtoMapper {
     public MovieSession toModel(MovieSessionRequestDto movieSessionRequestDto) {
         MovieSession movieSession = new MovieSession();
         movieSession.setShowTime(movieSessionRequestDto.getShowTime());
-        movieSession.setMovie(movieService.get(movieSessionRequestDto.getMovieId()));
-        movieSession.setCinemaHall(cinemaHallService.get(movieSessionRequestDto.getCinemaHallId()));
+        movieSession.setMovie(new Movie(movieSessionRequestDto.getMovieId()));
+        movieSession.setCinemaHall(new CinemaHall(movieSessionRequestDto.getCinemaHallId()));
         return movieSession;
     }
 }
